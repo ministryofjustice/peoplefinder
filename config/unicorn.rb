218161974de3ac089734b30cbf7aaef8a567bc08
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-05-07 11:41:36 +0100

    config for unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,14 @@
+worker_processes 4
+listen "/home/vagrant/pq/shared/unicorn.sock", :backlog => 64
+working_directory "/home/vagrant/pq/current"
+pid "/home/vagrant/pq/shared/pids/unicorn.pid"
+timeout 30
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
