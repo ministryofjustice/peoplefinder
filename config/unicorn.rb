7ee8d27b552a53e0f8e123141efd44afc6fcd341
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-05-07 12:50:35 +0100

    setup the logs paths

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,8 @@ worker_processes 4
 listen "/home/vagrant/pq/shared/unicorn.sock", :backlog => 64
 working_directory "/home/vagrant/pq/current"
 pid "/home/vagrant/pq/shared/unicorn.pid"
+stderr_path "/home/vagrant/pq/shared/unicorn.stderr.log"
+stdout_path "/home/vagrant/pq/shared/unicorn.stdout.log"
 timeout 30
 preload_app true
 
