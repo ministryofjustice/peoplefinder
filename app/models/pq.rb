b0085826c52fe35a465c7148bf67a301e7f9e2d0
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-05-15 13:46:56 +0100

    Updated validation of press/finance flags

--- app/models/pq.rb
+++ app/models/pq.rb
@@ -2,4 +2,6 @@ class PQ < ActiveRecord::Base
 	validates :pin , presence: true, uniqueness:true
 	validates :raising_member_id, presence:true
 	validates :question, presence:true
+ 	validates :press_interest, :inclusion => {:in => [true, false]}, if: :seen_by_press
+ 	validates :finance_interest, :inclusion => {:in => [true, false]}, if: :seen_by_finance
 end
