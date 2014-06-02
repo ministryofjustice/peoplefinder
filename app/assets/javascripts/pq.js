ef8d62b7449f9cca1b89223212e49601a92d252a
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-06-02 11:23:34 +0100

    improved UI for select AO

--- app/assets/javascripts/pq.js
+++ app/assets/javascripts/pq.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function () {
     $('#datetimepicker').datetimepicker();
-    $("#e1").select2();
+    $("#action_officers_pq_action_officer_id").select2({ width: '250px' });
 });
 
