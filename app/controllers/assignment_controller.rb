db31843fb824327ce663b99702260516b3f738ba
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-06-04 11:17:01 +0100

    first working version of the whole allocating process @dahernan, @colinbruce, @danielatjustice

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -3,31 +3,43 @@ class AssignmentController < ApplicationController
   before_action :set_data
  
   def index
-  	
+
+    action = params[:response]
+
+    if action == 'accept'
+      accept
+    end
+
+    if action == 'reject'
+      reject
+    end
+
+    if action == 'transfer'
+      transfer
+    end
+
   end
-
-
-  def accept
-  	@assignment.update_attributes(accept: true)
-  	render 'index'
-  end
-
-  def reject
-  	@assignment.update_attributes(reject: true)
-  	render 'index'
-  end
-
-  def transfer
-  	@assignment.update_attributes(transfer: true)
-  	render 'index'
-  end
-
 
   private
     def set_data
       @question = PQ.find_by(uin: params[:uin])
-      @ao = ActionOfficer.find_by(email: params[:email])
+      @ao = ActionOfficer.find_by(email: params[:entity])
       @assignment = ActionOfficersPq.where(pq_id: @question.id, action_officer_id: @ao.id).first
+    end
+
+    def accept
+      flash[:notice] = 'The Question is accepted'
+      @assignment.update_attributes(accept: true, reject: false, transfer: false)
+    end
+
+    def reject
+      flash[:notice] = 'The Question is rejected'
+      @assignment.update_attributes(accept: false, reject: true, transfer: false)
+    end
+
+    def transfer
+      flash[:notice] = 'The Question is mark for transfer'
+      @assignment.update_attributes(accept: false, reject: false, transfer: true)
     end
 
 end
