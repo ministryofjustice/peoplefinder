fc673e699382e5aa4859a5ffe252eef406818565
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-06-05 17:28:16 +0100

    basic hide/show for the reason of reject the question form

--- app/assets/javascripts/pq.js
+++ app/assets/javascripts/pq.js
@@ -2,5 +2,12 @@
 $(document).ready(function () {
     $('#datetimepicker').datetimepicker();
     $("#action_officers_pq_action_officer_id").select2({ width: '250px' });
+
+    $('#response_accept').click(function (){
+        $('#reason-textarea').addClass('hide');
+    })
+    $('#response_reject').click(function (){
+        $('#reason-textarea').removeClass('hide');
+    })
 });
 
