c4374754cb77e9195b03df44a7af898ea06e132f
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-06-06 16:00:30 +0100

    fixed again the element in javascript

--- app/assets/javascripts/pq.js
+++ app/assets/javascripts/pq.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
     $('#datetimepicker').datetimepicker();
     $("#action_officers_pq_action_officer_id").select2({ width: '250px' });
 
-    $('#allocation_response_response_accept').click(function (){
+    $('#allocation_response_response_action_accept').click(function (){
         $('#reason-textarea').addClass('hide');
     })
-    $('#allocation_response_response_reject').click(function (){
+    $('#allocation_response_response_action_reject').click(function (){
         $('#reason-textarea').removeClass('hide');
     })
 });
