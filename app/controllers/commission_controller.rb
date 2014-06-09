1074f431f14897a4eaa62a5fbf4a0aaeaa8d4146
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2014-06-09 13:47:49 +0100

    some error handling and validation @danielatdigital @dahernan

--- app/controllers/commission_controller.rb
+++ app/controllers/commission_controller.rb
@@ -1,6 +1,5 @@
 class CommissionController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_pq, only: [:show, :update]
 
   def commission
     @pq = PQ.find_by(uin: params[:id])
@@ -16,35 +15,37 @@ class CommissionController < ApplicationController
 
   def assign
     pq_id = params[:action_officers_pq][:pq_id]
-    # "action_officers_pq"=>{"action_officer_id"=>["", "1", "2"]
+    @pq = PQ.find(pq_id)
+    
+    comm = params[:action_officers_pq][:action_officer_id]
+    if comm.size==1 && comm.first.empty?
+      flash[:error] = "Please select at least one Action Officer"
+      return redirect_to action: 'commission', id: @pq.uin
+    end
+    
     params[:action_officers_pq][:action_officer_id].each do |ao_id| 
       if !ao_id.empty? 
         assignment = ActionOfficersPq.new(pq_id: pq_id, action_officer_id: ao_id)
         comm_service = CommissioningService.new
         result = comm_service.send(assignment)
+        begin
+          if result.nil?
+            flash[:error] = "Error in commissioning to #{assignment.action_officer.name}"
+            return redirect_to action: 'commission', id: @pq.uin
+          end
+        rescue => e
+          flash[:error] = "#{e}"
+          return redirect_to action: 'commission', id: @pq.uin
+        end              
       end
-      # begin
-  #       if result.nil?
-  #         flash[:error] = "Error in commissioning to #{assignment.action_officer.name}"
-  #         return redirect_to action: 'commission', id: @pq.uin
-  #       end
-  #     rescue => e
-  #       flash[:error] = "#{e}"
-  #       return redirect_to action: 'commission', id: @pq.uin
-  #     end      
     end
     
-    @pq = PQ.find(pq_id)
     flash[:success] = "Successfully allocated #{@pq.uin}"
     return redirect_to controller: 'pqs', action: 'show', id: @pq.uin 
     
   end    
 
   private
-    def set_pq
-      @pq = PQ.find_by(uin: params[:id])
-    end
-
     def assignment_params
       # TODO: Check the permit again
       # params.require(:action_officers_pq).permit(:action_officer_id, :pq_id)
--- app/models/action_officers_pq.rb
+++ app/models/action_officers_pq.rb
@@ -1,4 +1,4 @@
 class ActionOfficersPq < ActiveRecord::Base
-	belongs_to :pq
+  belongs_to :pq
 	belongs_to :action_officer
 end
