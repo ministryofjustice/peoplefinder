ac18a3f8de3c6435f7f57e3b0463c5842c36fb5f
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-06-13 15:44:59 +0100

    fixed rspec test

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -12,7 +12,7 @@ feature 'Visit the dashboard an show the questions for the day' do
     context 'view' do
       scenario "can view the questions tabled for today" do
         visit '/dashboard'
-        expect(page).to have_content('Parliamentary Questions of the day')
+        expect(page).to have_content('Parliamentary Questions')
       end
     end
 
