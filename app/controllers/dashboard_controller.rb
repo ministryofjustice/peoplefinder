2befa07af8d980cc6f0b76bac5aef4387a9a3a31
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-06-16 13:45:52 +0100

    Added basic pagination to the queries and the view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,9 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   before_action :load_import_service
 
+  # TODO define the number of question per page 
+  @@per_page = 5
+
   def index
 
     #TODO refactor to not allways call import in the PQ API
@@ -9,14 +12,14 @@ class DashboardController < ApplicationController
     #@questions = result_imported[:questions]
 
     at_beginning_of_day = DateTime.now.at_beginning_of_day
-    @questions_today = PQ.where('created_at >= ?', at_beginning_of_day).order(:internal_deadline).load
+    @questions_today = PQ.where('created_at >= ?', at_beginning_of_day).paginate(:page => params[:page], :per_page => @@per_page).order(:internal_deadline).load
     @questions_pending_count = PQ.where('created_at < ?', at_beginning_of_day).count
   end
 
   def in_progress
     at_beginning_of_day = DateTime.now.at_beginning_of_day
     @questions_today_count = PQ.where('created_at >= ?', at_beginning_of_day).count
-    @questions_pending = PQ.where('created_at < ?', at_beginning_of_day).order(:internal_deadline).load
+    @questions_pending = PQ.where('created_at < ?', at_beginning_of_day).paginate(:page => params[:page], :per_page => @@per_page).order(:internal_deadline).load
   end
 
   def search
