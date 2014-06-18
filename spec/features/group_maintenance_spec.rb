be377f0292848eb9620a7ddfe18f21befc020d0b
Paul Battley <pbattley@gmail.com>
2014-06-18 14:18:45 +0100

    Add features for group creation

---
+++ spec/features/group_maintenance_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature "Group maintenance" do
+  before do
+    log_in_as 'test.user@digital.justice.gov.uk'
+  end
+
+  scenario "Creating a top-level organisation" do
+    name = "An Organisation"
+    visit new_group_path
+    fill_in "Name", with: name
+    click_button "Create Group"
+
+    org = Group.find_by_name(name)
+    expect(org.name).to eql(name)
+    expect(org.parent).to eql(Department.instance)
+  end
+
+  scenario "Creating a team inside an organisation" do
+    org = create(:group)
+
+    name = "A Team"
+    visit new_group_path
+    fill_in "Name", with: name
+    select org.name, from: "Parent"
+    click_button "Create Group"
+
+    team = Group.find_by_name(name)
+    expect(team.name).to eql(name)
+    expect(team.parent).to eql(org)
+  end
+end
+
--- spec/support/login.rb
+++ spec/support/login.rb
@@ -3,5 +3,18 @@ module SpecSupport
     def mock_logged_in_user
       controller.session[:current_user] = User.new('test.user@digital.moj.gov.uk')
     end
+
+    def log_in_as(email)
+      OmniAuth.config.test_mode = true
+
+      OmniAuth.config.mock_auth[:google_apps] = OmniAuth::AuthHash.new({
+        provider: 'google_apps',
+        info: {
+          email: email
+        }
+      })
+
+      visit 'auth/google_apps'
+    end
   end
 end
