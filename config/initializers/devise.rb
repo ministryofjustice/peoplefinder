24b7af7ee95292d1ae033e2c76b74f09410d2972
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-06-18 13:21:44 +0100

    sign_out via GET /users/sign_out

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -295,6 +295,8 @@ Devise.setup do |config|
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
   # end
 
+  config.sign_out_via = :get
+
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
   # is mountable, there are some extra configurations to be taken into account.
