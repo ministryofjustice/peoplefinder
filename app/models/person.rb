7065df7dcbb90a465942a6824859c6d743264931
Toby Privett <toby@snaplab.co.uk>
2014-06-19 15:52:43 +0100

    List people by name

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,7 @@
 class Person < ActiveRecord::Base
   validates_presence_of :surname
+
+  def name
+    [given_name, surname].compact.join(' ')
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Person, :type => :model do
+  let(:person) { build(:person) }
   it { should validate_presence_of(:surname) }
+
+  describe '.name' do
+    before { person.surname = 'von Brown' }
+
+    context 'with a given_name and surname' do
+      it 'should concatenate given_name and surname' do
+        person.given_name = 'Jon'
+        expect(person.name).to eql('Jon von Brown')
+      end
+    end
+
+    context 'with a surname only' do
+      it 'should use thesurname' do
+        expect(person.name).to eql('von Brown')
+      end
+    end
+  end
 end
