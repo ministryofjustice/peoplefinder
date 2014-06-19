14e5ec8dacb0bed42808a8a9b2102e158515c465
Paul Battley <pbattley@gmail.com>
2014-06-19 10:59:03 +0100

    Add description to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -69,6 +69,6 @@ class GroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params.require(:group).permit(:parent_id, :name)
+      params.require(:group).permit(:parent_id, :name, :description)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619090527) do
+ActiveRecord::Schema.define(version: 20140619095316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140619090527) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
+    t.text     "description"
   end
 
   add_index "groups", ["parent_id"], name: "index_groups_on_parent_id", using: :btree
