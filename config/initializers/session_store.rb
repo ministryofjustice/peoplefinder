90a16eb2bcd29a218a1631e8a1beb0c26be4c23d
Toby Privett <toby@snaplab.co.uk>
2014-06-25 11:12:37 +0100

    Rename the cookie_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_peoplefinder_session_cookie_killer'
+Rails.application.config.session_store :cookie_store, key: '_moj_peoplefinder_session'
