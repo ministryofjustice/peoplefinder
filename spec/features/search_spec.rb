964f07997b7b6de8e4d09ab698d8a124d517aaa3
Toby Privett <toby@snaplab.co.uk>
2014-06-25 15:26:21 +0100

    Refactor search specs
    - add support for testing across the Person model

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,60 +1,24 @@
 require 'rails_helper'
 
 feature 'Search for people' do
-  describe 'with elasticsearch', elastic: true do
+  describe 'with elasticsearch' do
     before(:all) do
-      jon = Person.create!(given_name: 'Jon', surname: 'Browne')
-      jill = Person.create!(given_name: 'Jill', surname: 'Browning')
-      digital_services = Group.create!(name: 'Digital Services')
-      jill.memberships.create(group: digital_services, role: 'Cleaner')
+      Person.create!(given_name: 'Jon', surname: 'Browne')
       Person.import
       sleep 1
-    end
-
-    before(:each) do
       log_in_as 'test.user@digital.justice.gov.uk'
     end
 
     after(:all) do
-      Person.__elasticsearch__.create_index! index: Person.index_name, force: true
-      Group.delete_all
-      Person.delete_all
+      clean_up_indexes_and_tables
     end
 
-    scenario 'by surname' do
+    scenario 'in the most basic form' do
       visit root_path
       fill_in 'query', with: 'Browne'
       click_button 'Search'
 
       expect(page).to have_text('Jon Browne')
-      expect(page).to_not have_text('Jill Browning')
-    end
-
-    scenario 'by given_name and surname' do
-      visit root_path
-      fill_in 'query', with: 'Jill Browning'
-      click_button 'Search'
-
-      expect(page).to_not have_text('Jon Browne')
-      expect(page).to have_text('Jill Browning')
-    end
-
-    scenario 'by group name' do
-      visit root_path
-      fill_in 'query', with: 'Digital Services'
-      click_button 'Search'
-
-      expect(page).to_not have_text('Jon Browne')
-      expect(page).to have_text('Jill Browning')
-    end
-
-    scenario 'by membership role' do
-      visit root_path
-      fill_in 'query', with: 'Cleaner'
-      click_button 'Search'
-
-      expect(page).to_not have_text('Jon Browne')
-      expect(page).to have_text('Jill Browning')
     end
   end
 end
---
+++ spec/models/people_search_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe 'People Search', elastic: true do
+  before(:all) do
+    @alice = Person.create!(given_name: 'Alice', surname: 'Andrews')
+    @bob = Person.create!(given_name: 'Bob', surname: 'Browning', location: 'Petty France 10th floor', description: 'weekends only')
+    @digital_services = Group.create!(name: 'Digital Services')
+    @bob.memberships.create(group: @digital_services, role: 'Cleaner')
+    Person.import
+    sleep 1
+  end
+
+  after(:all) do
+    clean_up_indexes_and_tables
+  end
+
+  it 'should search by surname' do
+    results = search_for('Andrews')
+    expect(results).to include(@alice)
+    expect(results).to_not include(@bob)
+  end
+
+  it 'should search by name' do
+    results = search_for('Alice')
+    expect(results).to include(@alice)
+    expect(results).to_not include(@bob)
+  end
+
+  it 'should search by given_name' do
+    results = search_for('Bob Browning')
+    expect(results).to_not include(@alice)
+    expect(results).to include(@bob)
+  end
+
+  it 'should search by group name and membership role' do
+    results = search_for('Cleaner at digiTAL Services')
+    expect(results).to_not include(@alice)
+    expect(results).to include(@bob)
+  end
+
+  it 'should search by description and location' do
+    results = search_for('weekends at petty france office')
+    expect(results).to_not include(@alice)
+    expect(results).to include(@bob)
+  end
+
+  def search_for(query)
+    Person.search(query).records
+  end
+end
