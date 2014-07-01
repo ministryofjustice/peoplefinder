d672d3ff596150d3626d6f2c0582d61b8d987ac8
Toby Privett <toby@snaplab.co.uk>
2014-07-01 13:56:41 +0100

    Add a test for the contact_details helper

--- spec/helpers/people_helper_spec.rb
+++ spec/helpers/people_helper_spec.rb
@@ -9,6 +9,16 @@ RSpec.describe PeopleHelper, :type => :helper do
     expect(day_symbol(:works_wednesday)).to eql("W")
   end
 
+  it 'should format contact details' do
+    person = create(:person, email: 'hello@example.com', phone: '0207 123 45678', mobile: '0777-999-999')
+    expect(contact_details(person)).to eql("<a href=\"mailto:hello@example.com\">hello@example.com</a><br/>0207 123 45678<br/>Mob: 0777-999-999")
+  end
+
+  it 'should format incomplete contact details' do
+    person = create(:person, mobile: '0777-999-999')
+    expect(contact_details(person)).to eql("Mob: 0777-999-999")
+  end
+
   it 'should return group breadcrumbs' do
     justice = create(:group, name: 'Justice')
     digital_service = create(:group, parent: justice, name: 'Digital Services')
