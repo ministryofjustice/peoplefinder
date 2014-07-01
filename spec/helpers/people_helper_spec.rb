d5e3cacccfa22dac92bb61c684f578eb62252c72
Toby Privett <toby@snaplab.co.uk>
2014-07-01 14:01:06 +0100

    Defining context in the people_helper_spec

--- spec/helpers/people_helper_spec.rb
+++ spec/helpers/people_helper_spec.rb
@@ -9,19 +9,23 @@ RSpec.describe PeopleHelper, :type => :helper do
     expect(day_symbol(:works_wednesday)).to eql("W")
   end
 
-  it 'should format contact details' do
-    person = create(:person, email: 'hello@example.com', phone: '0207 123 45678', mobile: '0777-999-999')
-    expect(contact_details(person)).to eql("<a href=\"mailto:hello@example.com\">hello@example.com</a><br/>0207 123 45678<br/>Mob: 0777-999-999")
+  context '#contact_details' do
+    it 'should correctly format complete contact details' do
+      person = create(:person, email: 'hello@example.com', phone: '0207 123 45678', mobile: '0777-999-999')
+      expect(contact_details(person)).to eql("<a href=\"mailto:hello@example.com\">hello@example.com</a><br/>0207 123 45678<br/>Mob: 0777-999-999")
+    end
+
+    it 'should correctly format incomplete contact details' do
+      person = create(:person, mobile: '0777-999-999')
+      expect(contact_details(person)).to eql("Mob: 0777-999-999")
+    end
   end
 
-  it 'should format incomplete contact details' do
-    person = create(:person, mobile: '0777-999-999')
-    expect(contact_details(person)).to eql("Mob: 0777-999-999")
-  end
-
-  it 'should return group breadcrumbs' do
-    justice = create(:group, name: 'Justice')
-    digital_service = create(:group, parent: justice, name: 'Digital Services')
-    expect(group_breadcrumbs(digital_service)).to eql("<a href=\"/groups/justice\">Justice</a> > <a href=\"/groups/digital-services\">Digital Services</a>")
+  context '#breadcrumbs' do
+    it 'should return group breadcrumbs' do
+      justice = create(:group, name: 'Justice')
+      digital_service = create(:group, parent: justice, name: 'Digital Services')
+      expect(group_breadcrumbs(digital_service)).to eql("<a href=\"/groups/justice\">Justice</a> > <a href=\"/groups/digital-services\">Digital Services</a>")
+    end
   end
 end
