22a29e3e69a38938a22a7242f6ad5f1f76f969f2
Toby Privett <toby@snaplab.co.uk>
2014-07-01 11:47:22 +0100

    First pass at styling the profile page

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -6,4 +6,12 @@ module PeopleHelper
   def day_symbol(symbol)
     I18n.t(symbol, scope: [:people, :day_symbols])
   end
+
+  def contact_details(person)
+    [
+      (mail_to(person.email) if person.email.present?),
+      (person.phone if person.phone.present?),
+      ("Mob: #{ person.mobile }" if person.mobile.present?)
+    ].compact.join('<br/>').html_safe
+  end
 end
