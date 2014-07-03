fbe40fde85342930584a2c53ed2c2b1c54c9dfe3
Toby Privett <toby@snaplab.co.uk>
2014-07-03 17:10:43 +0100

    Clean up carrierwave images in tests

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -23,3 +23,19 @@ module CarrierWave
     end
   end
 end
+
+if Rails.env.test?
+  ImageUploader
+  CarrierWave::Uploader::Base.descendants.each do |klass|
+    next if klass.anonymous?
+    klass.class_eval do
+      def cache_dir
+        "#{Rails.root}/spec/support/uploads/tmp"
+      end
+
+      def store_dir
+        "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      end
+    end
+  end
+end
--- spec/support/carrierwave.rb
+++ spec/support/carrierwave.rb
@@ -1,5 +1,11 @@
 module SpecSupport
   module Carrierwave
+    RSpec.configure do |config|
+      config.after(:suite) do
+        FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads"])
+      end
+    end
+
     def sample_image
       File.join(Rails.root, "spec", "fixtures", "placeholder.png")
     end
