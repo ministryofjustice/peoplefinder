d9d4e01bac22eb0c86a2132c4204485e7d6f6fb8
Toby Privett <toby@snaplab.co.uk>
2014-07-04 11:00:37 +0100

    Disable carrierwave image processing in tests

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,11 @@
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
+  if Rails.env.development?
     config.storage = :file
+
+  elsif Rails.env.test?
+    config.storage = :file
+    config.enable_processing = false
+
   else
     config.storage = :fog
     config.fog_credentials = {
