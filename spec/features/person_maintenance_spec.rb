d9f3dcaf0eba9452712e08e592ed5c0115015f0f
Toby Privett <toby@snaplab.co.uk>
2014-07-08 10:57:51 +0100

    Display image above upload button in edit profile

--- spec/features/person_maintenance_spec.rb
+++ spec/features/person_maintenance_spec.rb
@@ -124,7 +124,7 @@ feature "Person maintenance" do
   scenario 'Adding a profile image' do
     visit new_person_path
     fill_in 'Surname', with: person_attributes[:surname]
-    attach_file 'Image', sample_image
+    attach_file 'person[image]', sample_image
     click_button 'Create Person'
 
     person = Person.find_by_surname(person_attributes[:surname])
