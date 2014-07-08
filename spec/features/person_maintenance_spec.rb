c1df8483448955a09a1eab93b5127b5efc82ec5e
Toby Privett <toby@snaplab.co.uk>
2014-07-08 16:29:22 +0100

    Link to 'add a new group' from the person form

--- spec/features/person_maintenance_spec.rb
+++ spec/features/person_maintenance_spec.rb
@@ -95,6 +95,12 @@ feature "Person maintenance" do
     end
   end
 
+  scenario 'Editing a person and linking to the new group page' do
+    visit edit_person_path(create(:person, person_attributes))
+    click_link 'Add a new group'
+    expect(page).to have_text('New group')
+  end
+
   scenario 'Removing a group' do
      group = create(:group, name: 'Digital Justice')
      person = create(:person, person_attributes)
