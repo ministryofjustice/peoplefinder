215624b90ca93460814f19cfe33d7b98821f5ea4
Paul Battley <pbattley@gmail.com>
2014-07-10 14:48:48 +0100

    Update template for new white-on-black header

--- config/application.rb
+++ config/application.rb
@@ -25,22 +25,6 @@ module Peoplefinder
     # app title appears in the header bar
     config.app_title = 'People Finder'
 
-    # phase governs text indicators and highlight colours
-    # presumed values: alpha, beta, live
-    config.phase = 'alpha'
-
-    # known values: information, service
-    config.product_type = 'service'
-
-    config.proposition_title = 'peoplefinder'
-
-    # govbranding switches on or off the crown logo, full footer and NTA font
-    config.govbranding = false
-
-    # feedback_email is the address linked in the alpha/beta bar asking for feedback
-    config.feedback_email = 'test@example.com'
-    config.feedback_url = ''
-
     config.valid_login_domains = %w[
       digital.justice.gov.uk
       digital.cabinet-office.gov.uk
