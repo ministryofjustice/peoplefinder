2043600a394f2985d8d3cd104594e4c81472c489
Toby Privett <toby@snaplab.co.uk>
2014-07-14 09:36:44 +0100

    Style flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  FLASH_NOTICE_KEYS = %w[ error notice warning ]
+
+  def flash_messages
+    messages = flash.keys.map(&:to_s) & FLASH_NOTICE_KEYS
+    return if messages.empty?
+    content_tag(:div, class: 'inner-block') {
+      content_tag(:div, id: 'flash-messages') {
+        messages.map { |type|
+          content_tag(:div, class: "flash-message #{type.to_s}") {
+            flash[type]
+          }
+        }.join.html_safe
+      }
+    }
+  end
 end
