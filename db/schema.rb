3fd516c3ffbb6ecf83ef5e9f8f36f28deeb9bc31
Toby Privett <toby@snaplab.co.uk>
2014-07-15 10:04:54 +0100

    Add staff number to the user model
    - A civil service employee is assigned
    an immutable staff number that stays with
    them throughout their career.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711150506) do
+ActiveRecord::Schema.define(version: 20140715090213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,10 @@ ActiveRecord::Schema.define(version: 20140711150506) do
 
   create_table "users", force: true do |t|
     t.text     "name"
-    t.text     "email",      null: false
+    t.text     "email",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "staff_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
