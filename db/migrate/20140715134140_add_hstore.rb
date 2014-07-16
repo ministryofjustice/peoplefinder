bd08031a1ffb2b278097eca6fe328ac9a3117888
Toby Privett <toby@snaplab.co.uk>
2014-07-16 11:42:41 +0100

    Create the hstore extension
    - the original commit failed to include
    the CREATE EXTENSION command

--- db/migrate/20140715134140_add_hstore.rb
+++ db/migrate/20140715134140_add_hstore.rb
@@ -1,5 +1,11 @@
 class AddHstore < ActiveRecord::Migration
-  def change
+  def up
+    execute 'CREATE EXTENSION hstore'
     enable_extension "hstore"
+  end
+
+  def down
+    disable_extension "hstore"
+    execute 'DROP EXTENSION hstore'
   end
 end
