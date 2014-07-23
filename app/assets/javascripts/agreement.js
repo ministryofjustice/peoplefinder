81c9e4abf4cce51f5e6883c72bba2a97202bbbcd
Paul Battley <pbattley@gmail.com>
2014-07-23 10:44:29 +0100

    Make cloning work for any .cloneable section

--- app/assets/javascripts/agreement.js
+++ app/assets/javascripts/agreement.js
@@ -2,35 +2,37 @@
 /* global $ */
 (function(){
   var makeCloneable = function(selector) {
-    var scope = $(selector);
+    $(selector).each(function(_, element) {
+      var scope = $(element);
 
-    var update_remove_one_visibility = function() {
-      var hidden = scope.find('.cloneable').length < 2;
-      scope.find('.remove-one').toggleClass('hidden', hidden);
-    };
+      var update_remove_one_visibility = function() {
+        var hidden = scope.find('.cloneable-item').length < 2;
+        scope.find('.remove-one').toggleClass('hidden', hidden);
+      };
 
-    update_remove_one_visibility();
-
-    scope.find('.add-one').click(function(e) {
-      e.preventDefault();
-
-      var newContent = scope.find('.cloneable:first').clone();
-      scope.find('.cloneable:last').after(newContent);
-
-      scope.find('.cloneable:last input[type="text"]').val('');
-      scope.find('.cloneable:last textarea').text('');
       update_remove_one_visibility();
-    });
 
-    scope.find('.remove-one').click(function(e) {
-      e.preventDefault();
+      scope.find('.add-one').click(function(e) {
+        e.preventDefault();
 
-      scope.find('.cloneable:last').remove();
-      update_remove_one_visibility();
+        var newContent = scope.find('.cloneable-item:first').clone();
+        scope.find('.cloneable-item:last').after(newContent);
+
+        scope.find('.cloneable-item:last input[type="text"]').val('');
+        scope.find('.cloneable-item:last textarea').text('');
+        update_remove_one_visibility();
+      });
+
+      scope.find('.remove-one').click(function(e) {
+        e.preventDefault();
+
+        scope.find('.cloneable-item:last').remove();
+        update_remove_one_visibility();
+      });
     });
   };
 
   $(document).ready(function() {
-    makeCloneable('#budgetary-responsibilities');
+    makeCloneable('.cloneable');
   });
 })();
