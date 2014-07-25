acee46e92118ed9507721653d7f58d0055931670
James Darling <james@abscond.org>
2014-07-25 11:12:06 +0100

    Use S3 if ENV vars set

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,5 @@
 CarrierWave.configure do |config|
-  if Rails.env.development?
-    config.storage = :file
-
-  elsif Rails.env.test?
-    config.storage = :file
-    config.enable_processing = false
-
-  else
+  if ENV['S3_KEY'] && ENV['S3_SECRET'] && ENV['S3_BUCKET_NAME']
     config.storage = :fog
     config.fog_credentials = {
       provider: 'AWS',
@@ -14,6 +7,11 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV['S3_SECRET']
     }
     config.fog_directory  = ENV['S3_BUCKET_NAME']
+  elsif Rails.env.test?
+    config.storage = :file
+    config.enable_processing = false
+  else
+    config.storage = :file
   end
 end
 
