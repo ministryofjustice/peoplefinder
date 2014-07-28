bac912f041d361cd7c7dbaea3e6157305a660c00
James Darling <james@abscond.org>
2014-07-28 14:48:08 +0100

    add send grid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {
     :host => "moj-peoplefinder.herokuapp.com"
   }
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
