25fb6a416eb44ef8f06056c7c1cecc9977c164f7
Toby Privett <toby@snaplab.co.uk>
2014-08-01 11:39:03 +0100

    Cleanup the group browsing spec

--- spec/features/group_browsing_spec.rb
+++ spec/features/group_browsing_spec.rb
@@ -26,39 +26,44 @@ feature "Group browsing" do
   end
 
   scenario "An organisation with subteams and some people" do
-    add_named_people_to_group(org)
-    visit group_path(org)
+    current_group = org
+    add_people_to_group(names, current_group)
+    visit group_path(current_group)
 
-    expect(page).to have_text("Teams within #{ org.name }")
-    expect(page).to have_link("View all people in #{ org.name }")
+    expect(page).to have_text("Teams within #{ current_group.name }")
+    expect(page).to have_link("View all people in #{ current_group.name }")
   end
 
   scenario "An organisation with subteams and no people" do
-    visit group_path(org)
+    current_group = org
+    visit group_path(current_group)
 
-    expect(page).to have_text("Teams within #{ org.name }")
-    expect(page).not_to have_link("View all people in #{ org.name }")
+    expect(page).to have_text("Teams within #{ current_group.name }")
+    expect(page).not_to have_link("View all people in #{ current_group.name }")
   end
 
   scenario "An organisation with no subteams (leaf_node) and some people" do
-    add_named_people_to_group(leaf_node)
-    visit group_path(leaf_node)
+    current_group = leaf_node
+    add_people_to_group(names, current_group)
+    visit group_path(current_group)
 
-    expect(page).not_to have_text("Teams within #{ org.name }")
+    expect(page).not_to have_text("Teams within #{ current_group.name }")
+    expect(page).to have_text("People in #{ current_group.name }")
     names.each do |name|
       expect(page).to have_text(name.join(' '))
     end
   end
 
   scenario "An organisation with no subteams (leaf_node) and no people" do
+    current_group = leaf_node
     visit group_path(leaf_node)
 
-    expect(page).not_to have_text("Teams within #{ org.name }")
-    expect(page).not_to have_link("View all people in #{ org.name }")
+    expect(page).not_to have_text("Teams within #{ current_group.name }")
+    expect(page).not_to have_link("View all people in #{ current_group.name }")
   end
 end
 
-def add_named_people_to_group(group)
+def add_people_to_group(names, group)
   names.each do |gn, sn|
     person = create(:person, given_name: gn, surname: sn)
     membership = create(:membership, person: person, group: group)
