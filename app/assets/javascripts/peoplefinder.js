d2ca49a279c859fa5d5ccfc1a44cbf60537447e2
Toby Privett <toby@snaplab.co.uk>
2014-08-04 09:56:56 +0100

    Remove unnecessary javascript return

--- app/assets/javascripts/peoplefinder.js
+++ app/assets/javascripts/peoplefinder.js
@@ -1,22 +1,22 @@
 $(function() {
-  return $(document).on("click", "#add_membership", function(e) {
+  $(document).on("click", "#add_membership", function(e) {
     e.preventDefault();
-    return $.ajax({
+    $.ajax({
       url: this,
       success: function(data) {
         var el_to_add;
         el_to_add = $(data).html();
-        return $('#memberships').append(el_to_add);
+        $('#memberships').append(el_to_add);
       },
       error: function(data) {
-        return alert("Sorry, There Was An Error!");
+        alert("Sorry, There Was An Error!");
       }
     });
   });
 });
 
 $(function() {
-  return $(document).on("click", "#hide_people_link", function(e) {
+  $(document).on("click", "#hide_people_link", function(e) {
     e.preventDefault();
     $('#all_people').remove();
     $('#show_people_link').show();
