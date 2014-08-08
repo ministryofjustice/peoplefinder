3ea21bb3eb6ea970afcdc07f38bcf94712da32f6
Toby Privett <toby@snaplab.co.uk>
2014-08-08 13:39:41 +0100

    Display link to view_all people for any group
    (except leaf nodes)

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -4,7 +4,7 @@ module GroupsHelper
   end
 
   def display_view_all_people_link_for_group(group)
-    !group.leaf_node? && group.non_leaderships.present?
+    !group.leaf_node?
   end
 
   def display_all_people_in_group(group)
