c14664728c11ef87281babbe576696bac0eafb65
Toby Privett <toby@snaplab.co.uk>
2014-08-12 16:52:44 +0100

    Extract profile completion to a concern

---
+++ app/models/concerns/completion.rb
@@ -0,0 +1,25 @@
+module Completion
+  extend ActiveSupport::Concern
+
+  included do
+    COMPLETION_SCORE_FIELDS = [
+       :given_name,
+       :surname,
+       :email,
+       :primary_phone_number,
+       :secondary_phone_number,
+       :location,
+       :description,
+       :groups
+     ]
+
+     def completion_score
+       completed = COMPLETION_SCORE_FIELDS.map { |f| send(f).present? }
+       (100 * completed.select { |f| f }.length) / COMPLETION_SCORE_FIELDS.length
+     end
+
+     def incomplete?
+       completion_score < 100
+     end
+   end
+end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,7 @@
 class Person < ActiveRecord::Base
   extend FriendlyId
   include Searchable
+  include Completion
 
   DAYS_WORKED = [
     :works_monday,
@@ -39,26 +40,6 @@ class Person < ActiveRecord::Base
 
   def to_s
     name
-  end
-
-  COMPLETION_SCORE_FIELDS = [
-    :given_name,
-    :surname,
-    :email,
-    :primary_phone_number,
-    :secondary_phone_number,
-    :location,
-    :description,
-    :groups
-  ]
-
-  def completion_score
-    completed = COMPLETION_SCORE_FIELDS.map { |f| send(f).present? }
-    (100 * completed.select { |f| f }.length) / COMPLETION_SCORE_FIELDS.length
-  end
-
-  def incomplete?
-    completion_score < 100
   end
 
   def slug_source
