9c0fe899588b89205c95d18dba9cfb0d97d2e1fa
Paul Battley <pbattley@gmail.com>
2014-08-14 17:25:21 +0100

    Remove methods we're not using
    We can add them back later if we need them.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,16 +35,8 @@ private
     flash[type] = I18n.t(full_key, options)
   end
 
-  def error(partial_key, options = {})
-    i18n_flash :error, partial_key, options
-  end
-
   def notice(partial_key, options = {})
     i18n_flash :notice, partial_key, options
-  end
-
-  def warning(partial_key, options = {})
-    i18n_flash :warning, partial_key, options
   end
 
   def access_denied
