bc9150fc3bf5436b77fc47fd9dfae31c8a6e3063
Paul Battley <pbattley@gmail.com>
2014-08-19 17:56:33 +0100

    Migrate parent_id to ancestry
    Ancestry provides a method to perform this migration, but it doesn't
    work.

--- db/migrate/20140819091601_add_ancestry_to_groups.rb
+++ db/migrate/20140819091601_add_ancestry_to_groups.rb
@@ -1,14 +1,41 @@
 class AddAncestryToGroups < ActiveRecord::Migration
   class Group < ActiveRecord::Base
-    has_ancestry cache_depth: true
+    belongs_to :parent, class: Group
+    has_many :children, class: Group, foreign_key: 'parent_id'
+
+    def hierarchy
+      [].tap { |acc|
+        node = self
+        while node
+          acc.unshift node
+          node = node.parent
+        end
+      }
+    end
   end
 
   def change
     add_column :groups, :ancestry, :text
     add_column :groups, :ancestry_depth, :integer, default: 0, null: false
     add_index :groups, :ancestry
-    Group.build_ancestry_from_parent_ids!
-    Group.rebuild_depth_cache!
+
+    Group.all.each do |group|
+      ids = group.hierarchy.map(&:id)[0 .. -2]
+      if ids.empty?
+        Group.connection.execute <<-SQL
+          UPDATE groups
+          SET ancestry_depth = 0
+          WHERE id = #{group.id}
+        SQL
+      else
+        Group.connection.execute <<-SQL
+          UPDATE groups
+          SET ancestry = '#{ids.join('/')}',
+              ancestry_depth = #{ids.length}
+          WHERE id = #{group.id}
+        SQL
+      end
+    end
     remove_column :groups, :parent_id
   end
 end
