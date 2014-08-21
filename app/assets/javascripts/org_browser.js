89df0cb2333e5f522ccee9bce2b0345961ae9728
Paul Battley <pbattley@gmail.com>
2014-08-21 12:28:58 +0100

    Refactor org browser
    Make it clearer what is public and what isn't, and where initialisation
    is happening.

--- app/assets/javascripts/org_browser.js
+++ app/assets/javascripts/org_browser.js
@@ -10,11 +10,35 @@ peoplefinderApp.injectNewContainer = function(container) {
 };
 
 peoplefinderApp.controller('OrgBrowserCtrl', function($scope, $element, $http) {
+  var initialize = function() {
+    $scope.groups = [];
+    $scope.mapper = new FormInputMapper($element);
+    $scope.selectMode = $scope.mapper.active;
+    if ($scope.selectMode) { $scope.selectedId = $scope.mapper.getId(); }
+    $http.get('/org.json').success(jsonLoaded);
+  };
+
+  var jsonLoaded = function(tree) {
+    var current, path;
+
+    if ($scope.selectMode) {
+      current = parseInt($element.attr('data-current-id'), 10);
+    }
+
+    var orgTree = new OrgTree(tree, current);
+
+    if ($scope.selectMode && $scope.selectedId) {
+      path = orgTree.pathToNodeId($scope.selectedId);
+      $scope.select(path[path.length - 1]);
+    } else {
+      path = orgTree.pathToRoot();
+    }
+    path.forEach(function(group) { $scope.moveDown(group); });
+  };
+
   $scope.moveDown = function(group) {
     if (group.disabled) { return; }
-    if ($scope.isExpandable(group)) {
-      $scope.groups.push(group);
-    }
+    if ($scope.isExpandable(group)) { $scope.groups.push(group); }
     $scope.select(group);
   };
 
@@ -25,7 +49,7 @@ peoplefinderApp.controller('OrgBrowserCtrl', function($scope, $element, $http) {
   };
 
   $scope.isExpandable = function(group) {
-    return group.children.length > 0;
+    return !!group.children.length;
   };
 
   $scope.isSelected = function(group) {
@@ -42,31 +66,5 @@ peoplefinderApp.controller('OrgBrowserCtrl', function($scope, $element, $http) {
     { get: function() { return $scope.groups[$scope.groups.length - 1]; } }
   );
 
-  $scope.groups = [];
-
-  $scope.mapper = new FormInputMapper($element);
-
-  if ($scope.mapper.active) {
-    $scope.selectMode = true;
-    $scope.selectedId = $scope.mapper.getId();
-  }
-
-  $http.get('/org.json').success(function(tree) {
-    var current;
-
-    if ($scope.selectMode) {
-      current = parseInt($element.attr('data-current-id'), 10);
-    }
-
-    var orgTree = new OrgTree(tree, current);
-
-    var path;
-    if ($scope.selectMode && $scope.selectedId) {
-      path = orgTree.pathToNodeId($scope.selectedId);
-      $scope.select(path[path.length - 1]);
-    } else {
-      path = orgTree.pathToRoot();
-    }
-    path.forEach(function(group) { $scope.moveDown(group); });
-  });
+  initialize();
 });
