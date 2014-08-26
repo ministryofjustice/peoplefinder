0bc6689baa8de09853f316a0e83c18bcb7f6c918
Paul Battley <pbattley@gmail.com>
2014-08-26 17:44:09 +0100

    Test Token#object

--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Token, type: :model do
-  it "generates a token" do
+  it 'generates a token' do
     token = described_class.new
     expect(token.value).to match(/\A[a-z0-9\-]{36}\z/)
   end
 
-  it "preserves the same token after persisting" do
+  it 'preserves the same token after persisting' do
     token = described_class.new
     value = token.value
     token.save!
     token.reload
     expect(token.value).to eql(value)
+  end
+
+  it 'returns the user for its object' do
+    user = User.new
+    token = described_class.new(user: user)
+    expect(token.object).to eql(user)
   end
 end
