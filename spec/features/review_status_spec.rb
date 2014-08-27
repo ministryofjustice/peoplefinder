1f99d76848aa96c18dbedf90c304b06f7535ae16
Paul Battley <pbattley@gmail.com>
2014-08-27 17:07:33 +0100

    Show the status of my feedback responses

---
+++ spec/features/review_status_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'Review status' do
+  let(:me) { create(:user) }
+
+  before do
+    token = create(:token, user: me)
+    visit token_path(token)
+  end
+
+  scenario 'viewing the status of my feedback' do
+    create :review, subject: me, author_name: 'Foxtrot', status: 'no_response'
+    create :review, subject: me, author_name: 'Golf', status: 'accepted'
+    create :review, subject: me, author_name: 'Hotel', status: 'completed'
+
+    visit reviews_path
+
+    expect(page).to have_text('Foxtrot No response')
+    expect(page).to have_text('Golf Accepted')
+    expect(page).to have_text('Hotel Completed')
+  end
+end
