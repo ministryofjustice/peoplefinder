05979b6c9e586604ff57341b11cd1f77d695c8d2
Toby Privett <toby@snaplab.co.uk>
2014-08-27 14:50:42 +0100

    Feedback response is tested in its own feature

---
+++ spec/features/respond_to_feedback_request_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'Respond to feedback request' do
+  scenario 'Accept a feedback request' do
+    visit token_url(create(:review_token))
+
+    select 'accept', from: 'Status'
+    click_button 'Update'
+
+    expect(Review.last).to be_accepted
+  end
+
+  scenario 'Decline a feedback request' do
+    visit token_url(create(:review_token))
+
+    select 'decline', from: 'Status'
+    click_button 'Update'
+
+    expect(Review.last).to be_declined
+  end
+end
--- spec/features/review_maintenance_spec.rb
+++ spec/features/review_maintenance_spec.rb
@@ -9,68 +9,43 @@ feature 'Review maintenance' do
   end
 
   scenario 'Invite a new person to give me feedback' do
-    create_a_feedback_request
+    visit reviews_path
+    fill_in_feedback_request_form
 
     review = me.reviews_received.last
-    expect(review.author_name).to eql('Danny Boy')
-    expect(review.author_email).to eql('danny@example.com')
-    expect(review.relationship).to eql('Colleague')
+    check_review_attributes(review)
 
-    mail = last_email
-    expect(mail.subject).to eq('Request for feedback')
-
-    link = links_in_email(mail).first
-    expect(link).to eql(token_url(review.tokens.last))
+    check_mail_attributes(last_email, review)
   end
 
   scenario 'Invite a new person to give my managee feedback' do
     managee = create(:user, manager: me)
 
     visit user_reviews_path(managee)
+    fill_in_feedback_request_form
 
+    review = managee.reviews_received.last
+    check_review_attributes(review)
+
+    check_mail_attributes(last_email, review)
+  end
+
+  def fill_in_feedback_request_form
     fill_in 'Name', with: 'Danny Boy'
     fill_in 'Email', with: 'danny@example.com'
     fill_in 'Relationship', with: 'Colleague'
     click_button 'Create'
+  end
 
-    review = managee.reviews_received.last
+  def check_review_attributes(review)
     expect(review.author_name).to eql('Danny Boy')
     expect(review.author_email).to eql('danny@example.com')
     expect(review.relationship).to eql('Colleague')
+  end
 
-    mail = last_email
+  def check_mail_attributes(mail, review)
     expect(mail.subject).to eq('Request for feedback')
-
     link = links_in_email(mail).first
     expect(link).to eql(token_url(review.tokens.last))
-  end
-
-  scenario 'Accept a feedback request' do
-    create_a_feedback_request
-
-    visit token_url(Token.last)
-    select 'accept', from: 'Status'
-    click_button 'Update'
-
-    expect(Review.last).to be_accepted
-  end
-
-  scenario 'Decline a feedback request' do
-    create_a_feedback_request
-
-    visit token_url(Token.last)
-    select 'decline', from: 'Status'
-    click_button 'Update'
-
-    expect(Review.last).to be_declined
-  end
-
-  def create_a_feedback_request
-    visit reviews_path
-
-    fill_in 'Name', with: 'Danny Boy'
-    fill_in 'Email', with: 'danny@example.com'
-    fill_in 'Relationship', with: 'Colleague'
-    click_button 'Create'
   end
 end
