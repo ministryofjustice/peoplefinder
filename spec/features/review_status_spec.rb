d20c76bfb3c88903a74596901d532bb2675acc88
Paul Battley <pbattley@gmail.com>
2014-08-27 17:30:09 +0100

    Show the status of my managees' feedback responses

--- spec/features/review_status_spec.rb
+++ spec/features/review_status_spec.rb
@@ -19,4 +19,18 @@ feature 'Review status' do
     expect(page).to have_text('Golf Accepted')
     expect(page).to have_text('Hotel Completed')
   end
+
+  scenario "viewing the status of my managees' feedback" do
+    managee = create(:user, manager: me)
+
+    create :review, subject: managee, author_name: 'Foxtrot', status: 'no_response'
+    create :review, subject: managee, author_name: 'Golf', status: 'accepted'
+    create :review, subject: managee, author_name: 'Hotel', status: 'completed'
+
+    visit user_reviews_path(managee)
+
+    expect(page).to have_text('Foxtrot No response')
+    expect(page).to have_text('Golf Accepted')
+    expect(page).to have_text('Hotel Completed')
+  end
 end
