650ea97945a33075bc15e4145785c546641139e4
Paul Battley <pbattley@gmail.com>
2014-08-28 17:18:44 +0100

    Fix JS linting errors

--- app/assets/javascripts/scs_appraisals.js
+++ app/assets/javascripts/scs_appraisals.js
@@ -1,9 +1,10 @@
+/* global $ */
 $(function() {
   $(document).on('click', '#feedback_request_status_reject', function() {
     $(this.form).find('.rejection_reason_fields').removeClass('hidden');
   });
 
   $(document).on('click', '#feedback_request_status_accept', function() {
-    $(this.form).find('.rejection_reason_fields').addClass('hidden')
+    $(this.form).find('.rejection_reason_fields').addClass('hidden');
   });
 });
