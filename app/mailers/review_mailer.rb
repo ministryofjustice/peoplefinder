f6ba50c312495adac3a39eba2fbae294fa106a60
Paul Battley <pbattley@gmail.com>
2014-09-05 16:37:40 +0100

    Email customis{ed|able} invitation message

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -4,6 +4,7 @@ class ReviewMailer < ActionMailer::Base
   def feedback_request(review, token)
     @recipient_name = review.author_name
     @subject_name = review.subject.name
+    @invitation_message = review.invitation_message
     @token = token
     mail to: review.author_email
   end
