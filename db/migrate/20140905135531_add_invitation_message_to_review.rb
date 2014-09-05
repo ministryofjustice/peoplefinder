0bad20fb2035e3a482ca3e164692efeb8af0521b
Paul Battley <pbattley@gmail.com>
2014-09-05 16:46:25 +0100

    Make migration work with existing data
    I.e. don't add a null constraint when adding a column until you've
    populated it for existing records.

--- db/migrate/20140905135531_add_invitation_message_to_review.rb
+++ db/migrate/20140905135531_add_invitation_message_to_review.rb
@@ -1,5 +1,10 @@
 class AddInvitationMessageToReview < ActiveRecord::Migration
   def change
-    add_column :reviews, :invitation_message, :text, null: false
+    add_column :reviews, :invitation_message, :text
+    execute <<-SQL
+      UPDATE reviews
+      SET invitation_message = 'legacy record'
+    SQL
+    change_column_null :reviews, :invitation_message, false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140905135531) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "reviews", force: true do |t|
     t.integer "subject_id"
     t.text    "author_name"
