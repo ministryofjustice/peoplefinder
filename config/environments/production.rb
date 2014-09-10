4d0bd21e0c4db75df7449170a238930acf6493d5
Jonathan Conway <jonathan@jaikoo.com>
2014-09-10 11:07:14 +0100

    [FIX] Serve vendored images on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
