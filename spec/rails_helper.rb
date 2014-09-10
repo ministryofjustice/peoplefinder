58553c7921ddbb2fe45301905f0adb432dec6f9a
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-09-10 16:24:54 +0100

    Explicitly require shoulda-matchers
    Gem dependencies aren't required by default

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
+require 'shoulda-matchers'
 
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
