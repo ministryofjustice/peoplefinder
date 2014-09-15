a74746d66f15d9ad8e9b88492af62ab22b89fa82
Toby Privett <toby@snaplab.co.uk>
2014-09-15 16:18:29 +0100

    Move completion tests to concerns/spec

---
+++ spec/models/concerns/completion_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Completion' do
+
+  context 'completion score' do
+    it 'returns 0 if all fields are empty' do
+      person = Person.new
+      expect(person.completion_score).to eql(0)
+      expect(person).to be_incomplete
+    end
+
+    it 'returns 50 if half the fields are filled' do
+      person = Person.new(
+        given_name: 'Bobby',
+        surname: 'Tables',
+        email: 'user.example@digital.justice.gov.uk',
+        primary_phone_number: '020 7946 0123'
+      )
+      expect(person.completion_score).to eql(50)
+      expect(person).to be_incomplete
+    end
+
+    it 'returns 100 if all fields are filled' do
+      person = Person.new(
+        given_name: 'Bobby',
+        surname: 'Tables',
+        email: 'user.example@digital.justice.gov.uk',
+        primary_phone_number: '020 7946 0123',
+        secondary_phone_number: '07700 900123',
+        location: 'London',
+        description: 'I am a real person'
+      )
+      person.groups << build(:group)
+      expect(person.completion_score).to eql(100)
+      expect(person).not_to be_incomplete
+    end
+  end
+end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -22,40 +22,6 @@ RSpec.describe Person, type: :model do
     end
   end
 
-  context 'completion score' do
-    it 'returns 0 if all fields are empty' do
-      person = described_class.new
-      expect(person.completion_score).to eql(0)
-      expect(person).to be_incomplete
-    end
-
-    it 'returns 50 if half the fields are filled' do
-      person = described_class.new(
-        given_name: 'Bobby',
-        surname: 'Tables',
-        email: 'user.example@digital.justice.gov.uk',
-        primary_phone_number: '020 7946 0123'
-      )
-      expect(person.completion_score).to eql(50)
-      expect(person).to be_incomplete
-    end
-
-    it 'returns 100 if all fields are filled' do
-      person = described_class.new(
-        given_name: 'Bobby',
-        surname: 'Tables',
-        email: 'user.example@digital.justice.gov.uk',
-        primary_phone_number: '020 7946 0123',
-        secondary_phone_number: '07700 900123',
-        location: 'London',
-        description: 'I am a real person'
-      )
-      person.groups << build(:group)
-      expect(person.completion_score).to eql(100)
-      expect(person).not_to be_incomplete
-    end
-  end
-
   context 'slug' do
     it 'generates from the first part of the email address if present' do
       person = create(:person, email: 'user.example@digital.justice.gov.uk')
