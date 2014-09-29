7f0f3a5d3c5a58cabe68e37bb099459d47a99140
Toby Privett <toby@snaplab.co.uk>
2014-09-29 12:18:46 +0100

    Translate reminder mailer subjects

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -2,7 +2,7 @@ class ReminderMailer < ActionMailer::Base
   def inadequate_profile(person)
     @person = person
     @token = Token.for_person(@person)
-    mail to: @person.email, subject: 'Reminder: update your profile today'
+    mail to: @person.email
   end
 
   def information_request(information_request)
@@ -10,8 +10,7 @@ class ReminderMailer < ActionMailer::Base
     @token = Token.for_person(@person)
     @message = information_request.message
 
-    mail to: @person.email,
-         subject: 'Request to update your People Finder profile'
+    mail to: @person.email
   end
 
   def reported_profile(reported_profile)
@@ -20,7 +19,6 @@ class ReminderMailer < ActionMailer::Base
     @reason_for_reporting = reported_profile.reason_for_reporting
     @additional_details = reported_profile.additional_details
 
-    mail to: reported_profile.recipient_email,
-         subject: 'A People Finder profile has been reported'
+    mail to: reported_profile.recipient_email
   end
 end
