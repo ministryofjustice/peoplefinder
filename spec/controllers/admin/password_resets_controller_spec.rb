066b22132f7cc4f6e26b4375ce6bbe1f2abc6987
Najaf Ali <ali.najaf@gmail.com>
2014-10-08 16:11:18 +0100

    Fixes PasswordResetsController spec

--- spec/controllers/admin/password_resets_controller_spec.rb
+++ spec/controllers/admin/password_resets_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Admin::PasswordResetsController, type: :controller do
   describe 'POST create' do
     let(:admin_user) { create(:admin_user, identities: [create(:identity)]) }
     it 'redirects to admin login screen' do
-      post :create, email: admin_user.email
+      post :create, password_reset: { email: admin_user.email }
       expect(flash[:notice]).to eq 'Password reset link sent'
       expect(response).to redirect_to(new_login_path)
     end
