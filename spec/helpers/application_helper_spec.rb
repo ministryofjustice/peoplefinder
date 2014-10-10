0c8d1344c9137f5ae640411e2229a00d790a6919
David Heath and Toby Privett <toby.privett+david.heath@digital.justice.gov.uk>
2014-10-10 14:48:50 +0100

    Fix whitespace

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Peoplefinder::ApplicationHelper, type: :helper do
 
-   let(:stubbed_time) { Time.new(2012, 10, 31, 2, 2, 2, "+01:00") }
+  let(:stubbed_time) { Time.new(2012, 10, 31, 2, 2, 2, "+01:00") }
 
   context '#last_update' do
     it 'shows last_update for a person' do
