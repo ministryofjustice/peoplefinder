39723a70e6a6c7d985ed336cbe1750948d466207
Toby Privett <toby@snaplab.co.uk>
2014-10-21 15:27:54 +0100

    Preset the tag_list in the select2 widget
    - Person#tag_list is stored in a hidden field
    
    - Ensure tagging.js only executes when #person_tag_chooser is present

--- app/assets/javascripts/peoplefinder/tagging.js
+++ app/assets/javascripts/peoplefinder/tagging.js
@@ -1,36 +1,40 @@
 $(window).load(function() {
-  var the_form = $('input[id=person_tags]').parents('form:first');
-  var initial_tags = $('#person_tags').attr('value');
 
-  // remove the non-js default field
-  $('.person_tags').remove();
+ if($('#person_tag_chooser').length > 0)
+  {
+    var the_form = $('input[id=person_tags]').parents('form:first');
+    var initial_tags = $('#person_tags').attr('value');
+    var tag_list = $('#tag_list').attr('value').split(',');
 
-  // add a hidden field to store the selected tags
-  $('<input>').attr({
-    type: 'hidden',
-    id: 'person_tags',
-    name: 'person[tags]',
-    value: initial_tags
-  }).appendTo(the_form);
+    $('.person_tags').remove();
 
-  // init the select2 widget
-  $('#person_tag_chooser').select2({
-    tags: [],
-    width: '480',
-    dropdownCssClass: 'form-control',
-    tokenSeparators: [','],
-  });
+    // add a hidden field to store the selected tags
+    $('<input>').attr({
+      type: 'hidden',
+      id: 'person_tags',
+      name: 'person[tags]',
+      value: initial_tags
+    }).appendTo(the_form);
 
-  // set the default state of the select2 widget
-  $('#person_tag_chooser').select2('val', [initial_tags]);
+    // init the select2 widget
+    $('#person_tag_chooser').select2({
+      tags: tag_list,
+      width: '480',
+      dropdownCssClass: 'form-control',
+      tokenSeparators: [','],
+    });
 
-  // enable adding a tag with the enter key
-  $('select2-search-field > input.select2-input').on('keyup', function(e) {
-    if(e.keyCode === 13) addToList($(this).val());
-  });
+    // set the default state of the select2 widget
+    $('#person_tag_chooser').select2('val', [initial_tags]);
 
-  // set the hidden field when the form is submitted
-  the_form.on('submit', function(e) {
-    $('#person_tags')[0].value = $('#person_tag_chooser').select2('val');
-  });
+    // enable adding a tag with the enter key
+    $('select2-search-field > input.select2-input').on('keyup', function(e) {
+      if(e.keyCode === 13) addToList($(this).val());
+    });
+
+    // set the hidden field when the form is submitted
+    the_form.on('submit', function(e) {
+      $('#person_tags')[0].value = $('#person_tag_chooser').select2('val');
+    });
+  }
 });
