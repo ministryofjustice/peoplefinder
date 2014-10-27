5bdc02b0f399c68f1e8fb5147a74f39c88e44a11
Toby Privett <toby@snaplab.co.uk>
2014-10-27 10:37:02 +0000

    Updates to the profile completeness
    - Secondary phone number is no longer required for
    a profile to be complete
    
    - Fixes inadequate_profiles scope that wasn't 
    correctly checking the no_phone flag

--- app/models/peoplefinder/concerns/completion.rb
+++ app/models/peoplefinder/concerns/completion.rb
@@ -9,7 +9,6 @@ module Peoplefinder::Concerns::Completion
       :surname,
       :email,
       :primary_phone_number,
-      :secondary_phone_number,
       :location,
       :description,
       :groups,
@@ -19,7 +18,7 @@ module Peoplefinder::Concerns::Completion
     def completion_score_fields
       fields = COMPLETION_SCORE_FIELDS
       if no_phone
-        fields -= [:primary_phone_number, :secondary_phone_number]
+        fields -= [:primary_phone_number]
       end
       fields
     end
@@ -29,9 +28,12 @@ module Peoplefinder::Concerns::Completion
         where("
           COALESCE(image,'') = ''
           OR COALESCE(location,'') = ''
-          OR COALESCE(
-            primary_phone_number, secondary_phone_number,''
-          ) = ''"
+          OR
+          CASE
+            WHEN no_phone != true
+            THEN COALESCE(primary_phone_number,'') = ''
+          END
+          "
         ).order(:email)
       }
 
