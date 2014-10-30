4b6a7b9ed1a55e91c0ed56c406e05aaedaa0780d
Toby Privett <toby@snaplab.co.uk>
2014-10-30 14:29:04 +0000

    Use definition lists for profile details
    includes:
    - changing h4 / p tags to dt / dd
    - moving communities to a partial
    - tweaking css for inline breadcrumbs
    - removing contact_details helper method

--- app/helpers/peoplefinder/people_helper.rb
+++ app/helpers/peoplefinder/people_helper.rb
@@ -9,14 +9,6 @@ module Peoplefinder::PeopleHelper
     I18n.t(symbol, scope: [:peoplefinder, :people, :day_symbols])
   end
 
-  def contact_details(person)
-    [
-      (mail_to(person.email) if person.email?),
-      (person.primary_phone_number if person.primary_phone_number?),
-      (person.secondary_phone_number if person.secondary_phone_number?)
-    ].compact.join('<br/>').html_safe
-  end
-
   def profile_image_tag(person, source, options = {})
     image_tag(source, options.merge(alt: "Current photo of #{ person }"))
   end
--- spec/helpers/peoplefinder/people_helper_spec.rb
+++ spec/helpers/peoplefinder/people_helper_spec.rb
@@ -8,16 +8,4 @@ RSpec.describe Peoplefinder::PeopleHelper, type: :helper do
   it "returns a symbol for each day" do
     expect(day_symbol(:works_wednesday)).to eql("W")
   end
-
-  context '#contact_details' do
-    it 'correctly formats complete contact details' do
-      person = create(:person, email: 'hello@example.com', primary_phone_number: '0207 123 45678', secondary_phone_number: '0777-999-999')
-      expect(contact_details(person)).to eql("<a href=\"mailto:hello@example.com\">hello@example.com</a><br/>0207 123 45678<br/>0777-999-999")
-    end
-
-    it 'correctly formats incomplete contact details' do
-      person = create(:person, primary_phone_number: '0777-999-999')
-      expect(contact_details(person)).to match(/0777-999-999/)
-    end
-  end
 end
