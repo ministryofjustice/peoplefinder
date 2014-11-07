d57bb516c71cc764f5635d7af47a170689d09716
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-11-07 17:57:31 +0000

    Trigger search index updates when person is saved

--- app/models/peoplefinder/concerns/searchable.rb
+++ app/models/peoplefinder/concerns/searchable.rb
@@ -4,24 +4,8 @@ module Peoplefinder::Concerns::Searchable
   extend ActiveSupport::Concern
 
   included do
-    include Elasticsearch::Model
-    include Elasticsearch::Model::Callbacks
-
-    # Force a full re-index after update, the ActiveModel::Dirty tracking
-    # doesn't detect changes to community_name
-    after_commit -> { __elasticsearch__.index_document }, on: :update
-
-    index_name [Rails.env, model_name.collection.gsub(/\//, '-')].join('_')
-
-    def self.delete_indexes
-      __elasticsearch__.delete_index! index: Peoplefinder::Person.index_name
-    end
-
-    def as_indexed_json(_options = {})
-      as_json(
-        only: [:tags, :description, :location],
-        methods: [:name, :role_and_group, :community_name]
-      )
-    end
+    after_save ->(person) { SEARCH_INDEX.index(person) }, on: :update
+    after_save ->(person) { SEARCH_INDEX.index(person) }, on: :create
+    after_save ->(person) { SEARCH_INDEX.delete(person) },  on: :destroy
   end
 end
