8573c4d733644b91ff7eb5085eb15f43ca6318aa
Jonathan Conway <jonathan@jaikoo.com>
2014-11-07 15:59:57 +0000

    Resolved Rubocop errors

--- app/services/deployment.rb
+++ app/services/deployment.rb
@@ -1,23 +1,28 @@
 module Deployment
-  def info
-    { version_number: version_number, build_date: build_date, commit_id: commit_id, build_tag: build_tag }
-  end
+  class << self
+    def info
+      {
+        version_number: version_number,
+        build_date: build_date,
+        commit_id: commit_id,
+        build_tag: build_tag
+      }
+    end
 
-  def version_number
-    ENV['APPVERSION'] || 'unknown'
-  end
+    def version_number
+      ENV['APPVERSION'] || 'unknown'
+    end
 
-  def build_date
-    ENV['APP_BUILD_DATE'] || 'unknown'
-  end
+    def build_date
+      ENV['APP_BUILD_DATE'] || 'unknown'
+    end
 
-  def commit_id
-    ENV['APP_GIT_COMMIT'] || 'unknown'
-  end
+    def commit_id
+      ENV['APP_GIT_COMMIT'] || 'unknown'
+    end
 
-  def build_tag
-    ENV['APP_BUILD_TAG'] || 'unknown'
+    def build_tag
+      ENV['APP_BUILD_TAG'] || 'unknown'
+    end
   end
-
-  extend self
 end
