366111fcc7c7c411dee20ac406587ce1b597275d
Najaf Ali <ali.najaf@gmail.com>
2014-11-19 14:55:46 +0000

    Makes secure cookie config fail safe

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-secure_cookie = ENV.fetch('SECURE_COOKIES', 'true')
-secure_cookie = %w[1 true].include?(secure_cookie)
-
 Rails.application.config.session_store :cookie_store,
   key: '_scs_appraisals_session',
-  secure: secure_cookie
+  secure: ENV['SECURE_COOKIES'] != 'false'
