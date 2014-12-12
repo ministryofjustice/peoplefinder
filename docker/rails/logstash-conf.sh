c60203758ba5419d7683e0a8c52edd0937994cd8
Kyriakos Oikonomakos <kyriakos.oikonomakos@digital.justice.gov.uk>
2014-12-12 12:48:19 +0000

    run sidekiq consumers

--- docker/rails/logstash-conf.sh
+++ docker/rails/logstash-conf.sh
@@ -28,9 +28,17 @@ cat <<EOT
       add_field => [ "env",       "$ENV" ]
       add_field => [ "format",    "json" ]
     }
+    file {
+      path => "/rails/log/sidekiq.log"
+      type => "rails"
+      add_field => [ "project",   "$PROJECT" ]
+      add_field => [ "appserver", "rails" ]
+      add_field => [ "version",   "$APPVERSION" ]
+      add_field => [ "env",       "$ENV" ]
+    }
   }
   output {
-    redis { host => "$LOGSTASH_SERVER" data_type => "list" key => "logstash" }
+    redis { host => "$LOGSTASH_SERVER" data_type => "list" key => "logstash:$PROJECT:$ENV" }
   }
 
 EOT
