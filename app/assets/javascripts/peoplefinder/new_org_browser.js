520d78478a8a9231db137079f559a2a88c0947b2
Mehmet Duran <mehmet.duran@gmail.com>
2014-12-15 17:50:10 +0000

    Org browser sorted

--- app/assets/javascripts/peoplefinder/new_org_browser.js
+++ app/assets/javascripts/peoplefinder/new_org_browser.js
@@ -1,97 +1,31 @@
 $(function (){
   "use strict";
 
-  $('.org-browser').children('.team').first().addClass('root-node');
+  var $orgBrowser = $('.new-org-browser');
 
-  $('.org-browser').on('click', '.subteam-link', function(e){
+  $orgBrowser.children('.team').first().addClass('root-node');
+
+  $orgBrowser.on('click', '.subteam-link', function(e){
+    var $target = $(e.target);
+
+    if( $target.closest('li').hasClass('has-subteams') == false ){
+      return;
+    }
+
     e.preventDefault();
     e.stopPropagation();
 
-    var $target = $(e.target);
     var $subteam = $target.closest('p').siblings('.team');
 
-    $target.addClass('expanded');
-    $subteam.addClass('visible');
+    $target.parents('li').addClass('expanded');
+    $orgBrowser.find('.team').removeClass('visible');
+    $subteam.children().parents('.team').addClass('visible');
+
+    animateScroll();
   });
+
+  var animateScroll = function (){
+    $orgBrowser.animate({ scrollLeft: $orgBrowser.width() + 100 }, 400);
+  }
 });
 
-
-// /*
-// * @Author: dominichey
-// * @Date:   2014-11-27 11:42:20
-// * @Last Modified by:   dominichey
-// * @Last Modified time: 2014-12-02 18:22:43
-// *
-// * dummy/prototpye functionality for new org browser
-// */
-
-// 'use strict';
-
-// var newList = '<ul class="org-list">'+
-//   '<li class="org-list-item org-list-group-title">'+
-//     '<a title="Ministry of Justice" href="#" class="org-list-link">'+
-//       '<h4>Corporate Services</h4>'+
-//     '</a>'+
-//   '</li>'+
-//   '<li class="org-list-item org-list-team">'+
-//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
-//       '<p class="org-list-team-name">Generic Team Name</p>'+
-//     '</a>'+
-//   '</li>'+
-//   '<li class="org-list-item org-list-team">'+
-//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
-//       '<p class="org-list-team-name">Generic Team Name</p>'+
-//     '</a>'+
-//   '</li>'+
-//   '<li class="org-list-item org-list-team">'+
-//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
-//       '<p class="org-list-team-name">Generic Team Name</p>'+
-//     '</a>'+
-//   '</li>'+
-//   '<li class="org-list-item org-list-team">'+
-//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
-//       '<p class="org-list-team-name">Generic Team Name</p>'+
-//     '</a>'+
-//   '</li>'+
-//   '<li class="org-list-item org-list-team">'+
-//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
-//       '<p class="org-list-team-name">Generic Team Name</p>'+
-//     '</a>'+
-//   '</li>'+
-//   '<li class="org-list-item org-list-team">'+
-//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
-//       '<p class="org-list-team-name">Generic Team Name</p>'+
-//     '</a>'+
-//   '</li>'+
-//   '<li class="org-list-item org-list-team">'+
-//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
-//       '<p class="org-list-team-name">Generic Team Name</p>'+
-//     '</a>'+
-//   '</li>'+
-//   '<li class="org-list-item org-list-team">'+
-//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
-//       '<p class="org-list-team-name">Generic Team Name</p>'+
-//     '</a>'+
-//   '</li>'+
-//   '<li class="org-list-item org-list-team">'+
-//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
-//       '<p class="org-list-team-name">Generic Team Name</p>'+
-//     '</a>'+
-//   '</li>'+
-//   '<li class="org-list-item org-list-team">'+
-//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
-//       '<p class="org-list-team-name">Generic Team Name</p>'+
-//     '</a>'+
-//   '</li>'+
-// '</ul>';
-
-// $(function() {
-
-//   $('.org-browser').on('click', '.org-list-link', function(e) {
-//     e.preventDefault();
-//     var $this = $(this);
-//     $this.parent().toggleClass( 'org-list-trail' );
-//     $this.parent().parent().parent().append( newList );
-//   });
-
-// });
