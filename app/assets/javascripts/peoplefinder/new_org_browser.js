e3deebc4740c4bc14abfd48f7e1c42beaeaf0313
Mehmet Duran <mehmet.duran@gmail.com>
2014-12-15 11:19:36 +0000

    Org browser interim commit

--- app/assets/javascripts/peoplefinder/new_org_browser.js
+++ app/assets/javascripts/peoplefinder/new_org_browser.js
@@ -1,79 +1,92 @@
-/*
-* @Author: dominichey
-* @Date:   2014-11-27 11:42:20
-* @Last Modified by:   dominichey
-* @Last Modified time: 2014-12-02 18:22:43
-*
-* dummy/prototpye functionality for new org browser
-*/
-
-'use strict';
-
-var newList = '<ul class="org-list">'+
-  '<li class="org-list-item org-list-group-title">'+
-    '<a title="Ministry of Justice" href="#" class="org-list-link">'+
-      '<h4>Corporate Services</h4>'+
-    '</a>'+
-  '</li>'+
-  '<li class="org-list-item org-list-team">'+
-    '<a title="Generic Team Name" href="#" class="org-list-link">'+
-      '<p class="org-list-team-name">Generic Team Name</p>'+
-    '</a>'+
-  '</li>'+
-  '<li class="org-list-item org-list-team">'+
-    '<a title="Generic Team Name" href="#" class="org-list-link">'+
-      '<p class="org-list-team-name">Generic Team Name</p>'+
-    '</a>'+
-  '</li>'+
-  '<li class="org-list-item org-list-team">'+
-    '<a title="Generic Team Name" href="#" class="org-list-link">'+
-      '<p class="org-list-team-name">Generic Team Name</p>'+
-    '</a>'+
-  '</li>'+
-  '<li class="org-list-item org-list-team">'+
-    '<a title="Generic Team Name" href="#" class="org-list-link">'+
-      '<p class="org-list-team-name">Generic Team Name</p>'+
-    '</a>'+
-  '</li>'+
-  '<li class="org-list-item org-list-team">'+
-    '<a title="Generic Team Name" href="#" class="org-list-link">'+
-      '<p class="org-list-team-name">Generic Team Name</p>'+
-    '</a>'+
-  '</li>'+
-  '<li class="org-list-item org-list-team">'+
-    '<a title="Generic Team Name" href="#" class="org-list-link">'+
-      '<p class="org-list-team-name">Generic Team Name</p>'+
-    '</a>'+
-  '</li>'+
-  '<li class="org-list-item org-list-team">'+
-    '<a title="Generic Team Name" href="#" class="org-list-link">'+
-      '<p class="org-list-team-name">Generic Team Name</p>'+
-    '</a>'+
-  '</li>'+
-  '<li class="org-list-item org-list-team">'+
-    '<a title="Generic Team Name" href="#" class="org-list-link">'+
-      '<p class="org-list-team-name">Generic Team Name</p>'+
-    '</a>'+
-  '</li>'+
-  '<li class="org-list-item org-list-team">'+
-    '<a title="Generic Team Name" href="#" class="org-list-link">'+
-      '<p class="org-list-team-name">Generic Team Name</p>'+
-    '</a>'+
-  '</li>'+
-  '<li class="org-list-item org-list-team">'+
-    '<a title="Generic Team Name" href="#" class="org-list-link">'+
-      '<p class="org-list-team-name">Generic Team Name</p>'+
-    '</a>'+
-  '</li>'+
-'</ul>';
-
-$(function() {
-
-  $('.org-browser').on('click', '.org-list-link', function(e) {
+$(function (){
+  $('.org-browser').on('click', 'a', function(e){
     e.preventDefault();
-    var $this = $(this);
-    $this.parent().toggleClass( 'org-list-trail' );
-    $this.parent().parent().parent().append( newList );
-  });
+    e.stopPropagation();
 
+    $target = $(e.target);
+    window.falan = $target;
+    $subteam = $target.closest('li').siblings('.team');
+    console.log($subteam);
+  });
 });
+
+
+// /*
+// * @Author: dominichey
+// * @Date:   2014-11-27 11:42:20
+// * @Last Modified by:   dominichey
+// * @Last Modified time: 2014-12-02 18:22:43
+// *
+// * dummy/prototpye functionality for new org browser
+// */
+
+// 'use strict';
+
+// var newList = '<ul class="org-list">'+
+//   '<li class="org-list-item org-list-group-title">'+
+//     '<a title="Ministry of Justice" href="#" class="org-list-link">'+
+//       '<h4>Corporate Services</h4>'+
+//     '</a>'+
+//   '</li>'+
+//   '<li class="org-list-item org-list-team">'+
+//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
+//       '<p class="org-list-team-name">Generic Team Name</p>'+
+//     '</a>'+
+//   '</li>'+
+//   '<li class="org-list-item org-list-team">'+
+//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
+//       '<p class="org-list-team-name">Generic Team Name</p>'+
+//     '</a>'+
+//   '</li>'+
+//   '<li class="org-list-item org-list-team">'+
+//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
+//       '<p class="org-list-team-name">Generic Team Name</p>'+
+//     '</a>'+
+//   '</li>'+
+//   '<li class="org-list-item org-list-team">'+
+//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
+//       '<p class="org-list-team-name">Generic Team Name</p>'+
+//     '</a>'+
+//   '</li>'+
+//   '<li class="org-list-item org-list-team">'+
+//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
+//       '<p class="org-list-team-name">Generic Team Name</p>'+
+//     '</a>'+
+//   '</li>'+
+//   '<li class="org-list-item org-list-team">'+
+//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
+//       '<p class="org-list-team-name">Generic Team Name</p>'+
+//     '</a>'+
+//   '</li>'+
+//   '<li class="org-list-item org-list-team">'+
+//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
+//       '<p class="org-list-team-name">Generic Team Name</p>'+
+//     '</a>'+
+//   '</li>'+
+//   '<li class="org-list-item org-list-team">'+
+//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
+//       '<p class="org-list-team-name">Generic Team Name</p>'+
+//     '</a>'+
+//   '</li>'+
+//   '<li class="org-list-item org-list-team">'+
+//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
+//       '<p class="org-list-team-name">Generic Team Name</p>'+
+//     '</a>'+
+//   '</li>'+
+//   '<li class="org-list-item org-list-team">'+
+//     '<a title="Generic Team Name" href="#" class="org-list-link">'+
+//       '<p class="org-list-team-name">Generic Team Name</p>'+
+//     '</a>'+
+//   '</li>'+
+// '</ul>';
+
+// $(function() {
+
+//   $('.org-browser').on('click', '.org-list-link', function(e) {
+//     e.preventDefault();
+//     var $this = $(this);
+//     $this.parent().toggleClass( 'org-list-trail' );
+//     $this.parent().parent().parent().append( newList );
+//   });
+
+// });
