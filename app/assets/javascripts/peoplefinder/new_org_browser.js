bceea342d64a8ec9456e5c6434937ddff7f082c2
Mehmet Duran <mehmet.duran@gmail.com>
2014-12-22 14:25:48 +0000

    Fixes for problems on multiple org browsers

--- app/assets/javascripts/peoplefinder/new_org_browser.js
+++ app/assets/javascripts/peoplefinder/new_org_browser.js
@@ -1,47 +1,51 @@
 $(function (){
   "use strict";
 
-  var $orgBrowser = $('.new-org-browser');
   var $content = $('#content');
 
-  var animateScroll = function (){
+  var animateScroll = function ($orgBrowser){
     var visibles = $orgBrowser.find('.visible');
     var offset = visibles.length * visibles.width();
 
     $orgBrowser.animate({ scrollLeft: offset }, 400);
   }
 
-  var revealSubteam = function ( $target, $subteam ){
+  var revealSubteam = function ( $orgBrowser, $target, $subteam ){
     $target.parents('li').addClass('expanded');
     $orgBrowser.find('.team').removeClass('visible');
     $subteam.children().parents('.team').addClass('visible');
   }
 
-
-  if( $orgBrowser.hasClass('has-form') ){
-    var $checked = $orgBrowser.find('input:checked');
+  if( $('.new-org-browser.has-form').length > 0 ){
+    var $checked = $('.new-org-browser.has-form').find('input:checked');
     if( $checked.length > 0 ){
       $checked.parents('.team').addClass('visible');
     }
   }
 
   $content.on('click', '.show-editable-fields', function (e){
-    setTimeout(animateScroll, 0);
+    var $orgBrowser = $(e.target).closest('.editable-summary').siblings('.editable-fields').find('.new-org-browser');
+    setTimeout(function (){ animateScroll($orgBrowser) }, 0);
   });
 
   // title link
   $content.on('click', '.new-org-browser .team-link', function (e){
+    var $target = $(e.target);
+    var $orgBrowser = $(e.target).closest('.new-org-browser');
+
     // if it's a form
     if( $orgBrowser.hasClass('has-form') ){
         e.preventDefault();
         e.stopPropagation();
 
-        $(e.target).closest('h3').children('input').prop('checked', 'checked');
+        $target.closest('h3').children('input').prop('checked', 'checked');
     }
   });
 
   $content.on('click', '.new-org-browser .subteam-link', function(e){
     var $target = $(e.target);
+    var $orgBrowser = $(e.target).closest('.new-org-browser');
+
 
     if( $target.closest('li').hasClass('has-subteams') == false ){
       if( $orgBrowser.hasClass('has-form') ){
@@ -60,9 +64,9 @@ $(function (){
     e.stopPropagation();
 
     var $subteam = $target.closest('p').siblings('.team');
-    revealSubteam($target, $subteam);
+    revealSubteam($orgBrowser, $target, $subteam);
 
-    animateScroll();
+    animateScroll($orgBrowser);
   });
 });
 
