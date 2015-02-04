4337db8f6417e2ce45c2e530e54776ca78e8c05a
Najaf Ali <ali.najaf@gmail.com>
2015-02-04 13:35:46 +0000

    Adds more tests for viewing a profile audit

--- spec/features/person_audit_spec.rb
+++ spec/features/person_audit_spec.rb
@@ -1,20 +1,35 @@
 require 'rails_helper'
 
 feature 'View person audit' do
-  let(:email) { 'test.user@digital.justice.gov.uk' }
-  let!(:person) { create(:person) }
+  let(:super_admin_email)  { 'test.user@digital.justice.gov.uk' }
+  let!(:super_admin)  { create(:super_admin, email: super_admin_email) }
+
+  let(:description)  { 'The best person' }
+  let(:phone_number) { '55555555555' }
+  let!(:person)      { with_versioning { create(:person) } }
 
   before do
-    person.update_attributes description: 'The best person'
-    person.update_attributes primary_phone_number: '55555555555'
-     
-    # login as a super admin
-    create(:super_admin, email: email)
-    omni_auth_log_in_as(email)
+    with_versioning do
+      person.update description: 'The best person'
+      person.update primary_phone_number: '55555555555'
+    end
   end
 
-  scenario 'View a profile as an admin user' do
+  scenario 'View audit as an admin user' do
+    omni_auth_log_in_as(super_admin.email)
     visit person_path(person)
+
     expect(page).to have_selector 'table.audit tbody tr', count: 3
+    within 'table.audit tbody' do
+      expect(find('tr:nth-child(1)')).to have_text "primary_phone_number => #{phone_number}"
+      expect(find('tr:nth-child(2)')).to have_text "description => #{description}"
+      expect(find('tr:nth-child(3)')).to have_text "email => #{person.email}"
+    end
+  end
+
+  scenario 'Hide audit as regular user' do
+    omni_auth_log_in_as(person.email)
+    visit person_path(person)
+    expect(page).to_not have_selector 'table.audit'
   end
 end
