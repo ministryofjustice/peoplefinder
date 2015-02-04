7e70bc25a535203acc113ba998d5776a91a2be41
Jakub Novotny <novotny.jakub@gmail.com>
2015-02-04 17:37:13 +0000

    Refactor Sanitisable concern to use ActiveSupport class_attribute

--- app/models/peoplefinder/concerns/sanitisable.rb
+++ app/models/peoplefinder/concerns/sanitisable.rb
@@ -4,12 +4,14 @@ module Peoplefinder::Concerns::Sanitisable
   extend ActiveSupport::Concern
 
   included do
-    class_variable_set(:@@fields_to_sanitise, [])
+    class_attribute :fields_to_sanitise, instance_writer: false
+    self.fields_to_sanitise = []
+
     before_validation :sanitise!
   end
 
   def sanitise!
-    self.class.class_variable_get(:@@fields_to_sanitise).each do |field_name|
+    self.fields_to_sanitise.each do |field_name|
       field = send(field_name)
       field.strip! if field && field.respond_to?(:strip!)
     end
@@ -17,7 +19,7 @@ module Peoplefinder::Concerns::Sanitisable
 
   module ClassMethods
     def sanitise_fields(*fields)
-      class_variable_set(:@@fields_to_sanitise, fields)
+      self.fields_to_sanitise = fields
     end
   end
 end
