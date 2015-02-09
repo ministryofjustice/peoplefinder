c9a1212084ee77f920ff0b73a78bd4d924a6de1f
Jakub Novotny <novotny.jakub@gmail.com>
2015-02-09 12:35:28 +0000

    Refactor csv user imported to work with csv file handle

--- app/services/peoplefinder/csv_people_importer.rb
+++ app/services/peoplefinder/csv_people_importer.rb
@@ -2,19 +2,20 @@ require 'csv'
 
 module Peoplefinder
   class CsvPeopleImporter
-    COLUMNS = [:given_name, :surname, :email]
+    COLUMNS = %w[given_name surname email]
 
     attr_reader :errors
 
     def initialize(csv)
-      @csv = csv
+      @csv = CSV.new(csv, headers: true, return_headers: true)
+      @header_row = @csv.first
     end
 
     def valid?
       @errors = []
 
       if missing_columns.empty?
-        csv_rows.each do |row|
+        @csv.each do |row|
           person = Person.new(row.to_h.slice(*COLUMNS))
           unless person.valid?
             @errors << %(row "#{row.to_csv.strip}": #{person.errors.full_messages.join(', ')})
@@ -29,15 +30,8 @@ module Peoplefinder
 
   private
 
-    def csv_rows
-      CSV.new(@csv, headers: true, skip_blanks: true, header_converters: :symbol)
-    end
-
     def missing_columns
-      header_row = CSV.new(@csv,
-        headers: true, return_headers: true, header_converters: :symbol).first
-
-      COLUMNS.reject { |column| header_row.include?(column) }
+      COLUMNS.reject { |column| @header_row.include?(column) }
     end
   end
 end
