f50e780d2fecf90b3d112d66aa752e31277dde61
Paul Battley <pbattley@gmail.com>
2015-02-12 14:26:16 +0000

    Remove team responsibilities
    Removed from the form; removed from the database.

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211164821) do
+ActiveRecord::Schema.define(version: 20150212132353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 20150211164821) do
     t.datetime "updated_at"
     t.string   "slug",               limit: nil
     t.text     "description"
-    t.text     "responsibilities"
     t.text     "ancestry"
     t.integer  "ancestry_depth",                 default: 0, null: false
     t.text     "team_email_address"
