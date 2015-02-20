adc3e441507132393dc892e0fe05af5efd00f0c2
Mehmet Duran <mehmet.duran@gmail.com>
2015-02-20 15:45:10 +0000

    Added a word counter for textareas with the 'data-limit' attribute

---
+++ app/assets/javascripts/peoplefinder/wordlimit.js
@@ -0,0 +1,32 @@
+jQuery(function ($){
+  'use strict';
+
+  var $limitedTextAreas = $('textarea[data-limit]');
+  if( $limitedTextAreas.length > 0 ){
+    $limitedTextAreas.each(function (i, o){
+      var $textarea = $(o);
+      var limit = $textarea.data('limit');
+      var $counterBox = $(
+        '<p class="textarea-word-count form-hint">'
+        + 'Maximum ' + limit + ' characters, including spaces. '
+        + '<span class="chars-remaining">(' + limit + ' remaining)</span>'
+        + '<p/>'
+      );
+
+      var $counter = $counterBox.find('.chars-remaining');
+      $textarea.after($counterBox);
+
+      $textarea.keyup(function (){
+        var charsLeft = limit - $textarea.val().length;
+
+        if( charsLeft > 0 ){
+          $counterBox.removeClass('error')
+          $counter.html( '(' + charsLeft + ' remaining)');
+        }else {
+          $counterBox.addClass('error');
+          $counter.html('(' + (charsLeft * -1) + ' extra)');
+        }
+      });
+    });
+  }
+});
