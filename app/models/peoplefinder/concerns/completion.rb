ae36460bed90b0290f194445f73f1ec6b2f2e3cf
Paul Battley <pbattley@gmail.com>
2015-02-23 17:06:53 +0000

    Use constants for completion fields

--- app/models/peoplefinder/concerns/completion.rb
+++ app/models/peoplefinder/concerns/completion.rb
@@ -3,25 +3,24 @@ require 'peoplefinder'
 module Peoplefinder::Concerns::Completion
   extend ActiveSupport::Concern
 
+  ADEQUATE_FIELDS = [
+    :image, :location_in_building, :building, :city, :primary_phone_number
+  ]
+
+  COMPLETION_FIELDS = [
+    :given_name, :surname, :email, :location_in_building, :building, :city,
+    :primary_phone_number, :description, :groups, :image
+  ]
+
   included do
-    def completion_score_fields
-      [
-        :given_name, :surname, :email, :location_in_building, :building, :city,
-        :primary_phone_number, :description, :groups, :image
-      ]
-    end
-
     def self.inadequate_profiles
-      fields = [:image, :location_in_building, :building, :city,
-                :primary_phone_number]
-      criteria = fields.map { |f| "COALESCE(#{f}, '') = ''" }.join(' OR ')
-
+      criteria = ADEQUATE_FIELDS.map { |f| "COALESCE(#{f}, '') = ''" }.join(' OR ')
       where(criteria).order(:email)
     end
 
     def completion_score
-      completed = completion_score_fields.map { |f| send(f).present? }
-      (100 * completed.select { |f| f }.length) / completion_score_fields.length
+      completed = COMPLETION_FIELDS.map { |f| send(f).present? }
+      (100 * completed.select { |f| f }.length) / COMPLETION_FIELDS.length
     end
 
     def incomplete?
