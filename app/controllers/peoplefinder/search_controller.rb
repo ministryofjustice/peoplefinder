317e3bd59960ede9e56e76e06d50dc2a91a6e4a3
Mehmet Duran <mehmet.duran@gmail.com>
2015-02-25 18:26:15 +0000

    - New signin bar - Grouped add/edit links in a box on the top right corner - Add person only shows on team pages beyond the second level - Add person show on search

--- app/controllers/peoplefinder/search_controller.rb
+++ app/controllers/peoplefinder/search_controller.rb
@@ -1,6 +1,7 @@
 module Peoplefinder
   class SearchController < ApplicationController
     def index
+      @search = true
       @people = Person.fuzzy_search(params[:query]).records.limit(100)
     end
   end
