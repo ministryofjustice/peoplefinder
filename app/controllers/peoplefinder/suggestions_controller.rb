ac19a2091342fec23186c325de6425d541c58740
Paul Battley <pbattley@gmail.com>
2015-03-27 15:09:42 +0000

    Send emails in response to suggestions
    In order to achieve this, we must put a hash on the queue instead of the
    Suggestion object. Why? Well, see [1] if you really want to know, but
    the short answer is that ActiveJob is a mess, and can only
    (de-/)serialise a short list of blessed classes in a massive case
    statement. Now, if the serialiser were a separate object that could be
    injected, we might be able to do something about that, but, well, Rails.
    
    That leaves us with two choices: create a database record for every
    Suggestion, or enqueue it as a hash and rehydrate it (which is,
    thankfully, easy with Virtus). We might eventually want to do the former
    anyway, but for now the latter is less drastic.
    
    1. https://github.com/rails/rails/blob/31085a5cd47894b6fa397ca22fe8aa552d339ce8/activejob/lib/active_job/arguments.rb#L51-L71

--- app/controllers/peoplefinder/suggestions_controller.rb
+++ app/controllers/peoplefinder/suggestions_controller.rb
@@ -2,12 +2,16 @@ module Peoplefinder
   class SuggestionsController < ApplicationController
     def new
       @suggestion = Suggestion.new
+      @person     = Person.friendly.find(params[:person_id])
     end
 
     def create
       @suggestion = Suggestion.new(params[:suggestion])
       if @suggestion.valid?
-        render text: 'great!'
+        person = Person.friendly.find(params[:person_id])
+        @delivery_details =
+          SuggestionDelivery.deliver(person, current_user, @suggestion)
+        render :create
       else
         render :new
       end
---
+++ spec/controllers/peoplefinder/suggestions_controller_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Peoplefinder::SuggestionsController, type: :controller do
+  routes { Peoplefinder::Engine.routes }
+
+  before do
+    mock_logged_in_user
+  end
+
+  describe 'GET new' do
+    let(:person_id)  { 'person-id' }
+    let(:person)     { double('person') }
+    let(:suggestion) { double('suggestion') }
+    let(:friendly)   { double('friendly') }
+
+    it 'assigns a new suggestion as @suggestion' do
+      expect(Peoplefinder::Suggestion).to receive(:new).and_return(suggestion)
+      allow(Peoplefinder::Person).to receive(:friendly).and_return(friendly)
+      allow(friendly).to receive(:find).with(person_id).and_return(person)
+
+      get :new, person_id: person_id
+
+      expect(assigns(:suggestion)).to eq suggestion
+    end
+
+    it 'assigns loaded person to @person' do
+      allow(Peoplefinder::Suggestion).to receive(:new).and_return(suggestion)
+      expect(Peoplefinder::Person).to receive(:friendly).and_return(friendly)
+      expect(friendly).to receive(:find).with(person_id).and_return(person)
+
+      get :new, person_id: person_id
+
+      expect(assigns(:person)).to eq person
+    end
+  end
+
+  describe 'POST create' do
+    let(:params)     { { foo: 'bar' } }
+    describe 'invalid suggestion' do
+      let(:suggestion) { double('suggestion', :'valid?' => false) }
+
+      it 'renders the form' do
+        expect(Peoplefinder::Suggestion).to receive(:new).with(params).and_return(suggestion)
+        post :create, person_id: 'foo', suggestion: params
+        expect(response).to render_template(:new)
+      end
+    end
+
+    describe 'valid suggestion' do
+      let(:person_id)  { 'person-id' }
+      let(:person)     { double('person') }
+      let(:friendly)   { double('friendly') }
+      let(:suggestion) { double('suggestion', :'valid?' => true) }
+
+      it 'delivers the suggestion' do
+        expect(Peoplefinder::Suggestion).to receive(:new).with(params).and_return(suggestion)
+        expect(Peoplefinder::Person).to receive(:friendly).and_return(friendly)
+        expect(friendly).to receive(:find).with(person_id).and_return(person)
+        expect(Peoplefinder::SuggestionDelivery).to receive(:deliver).with(person, current_user, suggestion)
+
+        post :create, person_id: person_id, suggestion: params
+
+        expect(response).to render_template(:create)
+      end
+    end
+  end
+end
