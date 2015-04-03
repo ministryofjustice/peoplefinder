1c5a78b4adfd0d5db49279954cc95f9f4d75f3b9
Paul Battley <pbattley@gmail.com>
2015-04-03 16:47:45 +0100

    Import, improve & test ping endpoint from wrapper

---
+++ app/services/deployment.rb
@@ -0,0 +1,40 @@
+class Deployment
+  def self.info
+    Deployment.new.info
+  end
+
+  def initialize(env = ENV)
+    @env = env
+  end
+
+  def info
+    {
+      version_number: version_number,
+      build_date: build_date,
+      commit_id: commit_id,
+      build_tag: build_tag
+    }
+  end
+
+private
+
+  def version_number
+    lookup('APPVERSION')
+  end
+
+  def build_date
+    lookup('APP_BUILD_DATE')
+  end
+
+  def commit_id
+    lookup('APP_GIT_COMMIT')
+  end
+
+  def build_tag
+    lookup('APP_BUILD_TAG')
+  end
+
+  def lookup(key)
+    @env.fetch(key, 'unknown')
+  end
+end
