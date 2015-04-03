7f7af9e82d5ad580e890a219247519bbf0d2ba54
Paul Battley <pbattley@gmail.com>
2015-04-03 12:49:53 +0100

    Remove namespace from helpers

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,12 +1,10 @@
-require 'peoplefinder'
-
-module Peoplefinder::PeopleHelper
+module PeopleHelper
   def day_name(symbol)
-    I18n.t(symbol, scope: [:peoplefinder, :people, :day_names])
+    I18n.t(symbol, scope: [:people, :day_names])
   end
 
   def day_symbol(symbol)
-    I18n.t(symbol, scope: [:peoplefinder, :people, :day_symbols])
+    I18n.t(symbol, scope: [:people, :day_symbols])
   end
 
   def profile_image_tag(person, source, options = {})
--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,12 +1,10 @@
-require 'peoplefinder'
-
-module Peoplefinder::VersionsHelper
+module VersionsHelper
   def view_template(version)
     version.membership? ? 'membership' : 'general'
   end
 
   def link_to_edited_item(version)
-    if version.item && !version.item.is_a?(Peoplefinder::Membership)
+    if version.item && !version.item.is_a?(Membership)
       link_to(version.item, version.item)
     end
   end
