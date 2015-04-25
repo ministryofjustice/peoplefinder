8b7235d48b021c3204faf24eb83295fbf2ff191f
Paul Battley <pbattley@gmail.com>
2015-04-25 22:16:12 +0100

    Explicitly load Group with associated people
    Brakeman identified the previous implementation as possibly unsafe.
    Whilst I believe that this was a false positive, this two-step process -
    of finding the group via its slug within the collection, then loading
    the full graph including its people - does not have such a potential
    problem.
    
    We should revisit whether we actually need to load all the people within
    a group here, however: we almost certainly don't need it for
    edit-related actions.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -73,7 +73,8 @@ private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_group
-    @group = collection.friendly.includes(:people).find(params[:id])
+    group = collection.friendly.find(params[:id])
+    @group = Group.includes(:people).find(group.id)
   end
 
   def set_org_structure
