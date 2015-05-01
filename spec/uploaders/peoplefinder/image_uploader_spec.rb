df1cba4e2ed4df641fb56331c2409dfa7f540e45
Paul Battley <pbattley@gmail.com>
2015-05-01 15:09:57 +0100

    Refactor spec to use subject

--- spec/uploaders/peoplefinder/image_uploader_spec.rb
+++ spec/uploaders/peoplefinder/image_uploader_spec.rb
@@ -5,22 +5,23 @@ RSpec.describe ImageUploader, type: :uploader do
   include PermittedDomainHelper
 
   let(:person) { create(:person, image: File.open(sample_image)) }
+  subject { person.image }
 
   before do
     described_class.enable_processing = true
   end
 
   it 'creates default image sizes' do
-    expect(person.image.croppable).to be_no_larger_than(1024, 1024)
-    expect(person.image.medium).to be_no_larger_than(512, 512)
+    expect(subject.croppable).to be_no_larger_than(1024, 1024)
+    expect(subject.medium).to be_no_larger_than(512, 512)
   end
 
   it 'crops the medium image and leaves the croppable version intact' do
     person.assign_attributes(crop_x: 10, crop_y: 10, crop_w: 20, crop_h: 20)
-    person.image.recreate_versions!
+    subject.recreate_versions!
 
-    expect(person.image.croppable).to be_no_larger_than(1024, 1024)
-    expect(person.image.medium).to have_dimensions(20, 20)
+    expect(subject.croppable).to be_no_larger_than(1024, 1024)
+    expect(subject.medium).to have_dimensions(20, 20)
   end
 
   after do
