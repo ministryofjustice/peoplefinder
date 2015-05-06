a1ba1b609615f94b5a1d13277aa7d7a74d113e63
Paul Battley <pbattley@gmail.com>
2015-05-06 11:51:36 +0100

    Calculate completion in app
    This is less efficient, but we want to bucket the scores, and that means
    either loading all the records or duplicating a lot of calculation in
    SQL. The first choice is less complicated. This is only to be used for
    metrics, so it shouldn't affect day-to-day use.

--- app/models/concerns/completion.rb
+++ app/models/concerns/completion.rb
@@ -19,33 +19,7 @@ module Concerns::Completion
     end
 
     def self.overall_completion
-      #
-      # Calculate a total completion score. This is the total number of
-      # COMPLETION_FIELDS present across all people in the database.
-      #
-      # Note the oddity with groups: the presence of a group is actually a
-      # question of whether there are one or more memberships associated with
-      # the person. As all the other fields are text, we translate this into
-      # 'Y' or '' to work with the generic CASE + coalesce sum expression
-      # generated from COMPLETION_FIELDS.
-      #
-      sum_expression = COMPLETION_FIELDS.map { |f|
-        "CASE WHEN coalesce(#{f}, '') = '' THEN 0 ELSE 1 END"
-      }.join(' + ')
-      completed_fields = connection.execute(%{
-        SELECT sum(#{sum_expression}) AS completed FROM (
-          SELECT people.*,
-            CASE WHEN count(memberships.id) > 0 THEN 'Y' ELSE '' END AS groups
-          FROM people LEFT OUTER JOIN memberships
-          ON memberships.person_id = people.id
-          GROUP BY people.id) AS subquery
-      })[0]['completed'].to_f
-
-      # Turn it into a percentage. The denominator is the total number of
-      # completed fields possible, i.e. the number of people multiplied by the
-      # number of items in COMPLETION_FIELDS.
-      #
-      (completed_fields * 100.0) / (count * COMPLETION_FIELDS.length)
+      all.map(&:completion_score).inject(0.0, &:+) / count
     end
 
     def completion_score
