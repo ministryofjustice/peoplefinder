7d42da8c9b38606bf44b5dcf156e69f50d30b1c5
Jakub Novotny <novotny.jakub@gmail.com>
2015-07-31 11:07:22 +0100

    Add new Dockerfile and run.sh to spin up the app and worker

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+ROLE="${1:-app}"
+
+echo "1: ${1}"
+echo "ROLE: ${ROLE}"
+
+case ${DOCKER_STATE} in
+migrate)
+    echo "running migrate"
+    bundle exec rake db:migrate
+    ;;
+esac
+
+case ${ROLE} in
+worker)
+    echo "running worker"
+    bundle exec rake jobs:work
+    ;;
+*)
+    echo "running app"
+    bundle exec unicorn -c config/unicorn.rb -p $UNICORN_PORT
+    ;;
+esac
