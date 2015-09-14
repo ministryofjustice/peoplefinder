0191f8ed042bc006193e49ec1480daf766377a1e
Todd Tyree <tatyree@gmail.com>
2015-09-14 17:11:08 +0100

    Moves simplecov to rails_helper, ignores .bundle
    As per rspec best practice, I'm moving customisations to rails_helper. I
    tend to install gems in `.bundle`, so I'm adding that to the simplecov
    ignore list as well.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,11 @@
 require "codeclimate-test-reporter"
+require 'simplecov-rcov'
 CodeClimate::TestReporter.start
+SimpleCov.start 'rails' do
+  add_filter '/gem/'
+  add_filter '.bundle'
+end
+SimpleCov.minimum_coverage 100
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
