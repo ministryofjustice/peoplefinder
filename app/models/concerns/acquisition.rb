c889b76893ee9f137ef9d519533a37d582f0f220
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2015-11-13 11:46:47 +0000

    Add before and after options to acquisition metric
    Enables calculation of acquisition metric for a cohort of
    users based on their created_at date.

--- app/models/concerns/acquisition.rb
+++ app/models/concerns/acquisition.rb
@@ -3,14 +3,20 @@ module Concerns::Acquisition
 
   included do
     # % of people who have logged in at least once
-    def self.acquired_percentage
+    def self.acquired_percentage from: nil, before: nil
       people_count = Person.count
       if people_count == 0
         0.0
       else
-        acquired_count = Person.where("people.login_count > 0").count
-        (acquired_count.to_f / people_count * 100).round(0)
+        (acquired_count(from: from, before: before).to_f / people_count * 100).round(0)
       end
+    end
+
+    def self.acquired_count from: nil, before: nil
+      acquired = Person.where("people.login_count > 0")
+      acquired = acquired.where("created_at >= ?", from) if from
+      acquired = acquired.where("created_at < ?", before) if before
+      acquired.count
     end
 
   end
--- spec/models/concerns/acquisition_spec.rb
+++ spec/models/concerns/acquisition_spec.rb
@@ -31,5 +31,26 @@ RSpec.describe 'Acquisition' do
       expect(Person.acquired_percentage).to eq(67)
     end
 
+    context 'when one person created yesterday who has logged in' do
+      before do
+        create(:person, login_count: 1, created_at: Date.yesterday.to_time)
+      end
+
+      it 'returns 0 when from date is today' do
+        expect(Person.acquired_percentage(from: Date.today.to_s)).to eq(0)
+      end
+
+      it 'returns 100 when from date is yesterday' do
+        expect(Person.acquired_percentage(from: Date.yesterday.to_s)).to eq(100)
+      end
+
+      it 'returns 0 when before date is yesterday' do
+        expect(Person.acquired_percentage(before: Date.yesterday.to_s)).to eq(0)
+      end
+
+      it 'returns 100 when before date is today' do
+        expect(Person.acquired_percentage(before: Date.today.to_s)).to eq(100)
+      end
+    end
   end
 end
