cd287c9c6100f993ec3e2b128c954dc1b8c50feb
robmckinnon <rob@movingflow>
2015-11-26 12:10:56 +0000

    Change team leader checkbox text based on team selection
    On page load set the team name in the team leader checkbox
    to the current team selection.
    
    When team selection changes set the new team name in the
    team leader checkbox hint text.

--- spec/support/org_browser.rb
+++ spec/support/org_browser.rb
@@ -3,6 +3,7 @@ module SpecSupport
     def select_in_team_select(text)
       page.execute_script("$('select.team-select-enhanced').css({display: 'inherit'})")
       select text
+      page.execute_script("$('.team-select-enhanced').trigger('change')")
     end
 
     def click_on_team_in_org_browser(text)
