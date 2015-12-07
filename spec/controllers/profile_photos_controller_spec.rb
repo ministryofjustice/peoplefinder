62d53831b18272f769d140338ab01619af76746d
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2015-12-07 15:22:28 +0000

    Spec that photo response has MIME type set to "text"

--- spec/controllers/profile_photos_controller_spec.rb
+++ spec/controllers/profile_photos_controller_spec.rb
@@ -24,6 +24,12 @@ RSpec.describe ProfilePhotosController, type: :controller do
         post :create, profile_photo: valid_params
         expect(JSON.parse(response.body)['id']).to eq photo_id
       end
+
+      it 'sets return MIME type to "text" to allow iframe target to work with > IE8' do
+        allow(ProfilePhoto).to receive(:create).and_return(photo)
+        post :create, profile_photo: valid_params
+        expect(response.header['Content-Type']).to eq 'text/html; charset=utf-8'
+      end
     end
   end
 end
