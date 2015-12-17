10329193d979ffd7008670b551182a34b7aac821
robmckinnon <rob@movingflow>
2015-12-17 09:36:14 +0000

    Add never_logged_in reminder email to ReminderMailer
    To be sent to remind users that have never logged in
    to update their profile.

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -7,4 +7,11 @@ class ReminderMailer < ActionMailer::Base
     @edit_url = edit_person_url(@person)
     mail to: @person.email
   end
+
+  def never_logged_in(person)
+    @person = person
+    token = Token.for_person(person)
+    @token_url = token_url(token)
+    mail to: @person.email
+  end
 end
--- spec/mailers/reminder_mailer_spec.rb
+++ spec/mailers/reminder_mailer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ReminderMailer do
   include PermittedDomainHelper
 
-  let(:person) { create(:person, email: 'test.user@digital.justice.gov.uk') }
+  let(:person) { create(:person, given_name: 'John', email: 'test.user@digital.justice.gov.uk') }
 
   shared_examples 'sets email to and from correctly' do
     it 'sets the sender' do
@@ -30,9 +30,17 @@ RSpec.describe ReminderMailer do
   end
 
   shared_examples 'includes link to edit person' do
-    it 'includes the the person edit url' do
-      %w(plain html).each do |part_type|
+    it 'includes the person edit url' do
+      %w[plain html].each do |part_type|
         expect(get_message_part(mail, part_type)).to have_text(edit_person_url(person))
+      end
+    end
+  end
+
+  shared_examples 'includes link to token login' do
+    it 'includes the token login url' do
+      %w[plain html].each do |part_type|
+        expect(get_message_part(mail, part_type)).to have_text('http://www.example.com/tokens/')
       end
     end
   end
@@ -47,6 +55,18 @@ RSpec.describe ReminderMailer do
     include_examples 'body contains', "profile is 33% complete"
 
     include_examples 'includes link to edit person'
+  end
+
+  describe '.never_logged_in' do
+    let(:mail) { described_class.never_logged_in(person).deliver_now }
+
+    include_examples 'sets email to and from correctly'
+
+    include_examples 'subject contains', 'Reminder: update your profile today'
+
+    include_examples 'body contains', 'Hello John'
+
+    include_examples 'includes link to token login'
   end
 
 end
