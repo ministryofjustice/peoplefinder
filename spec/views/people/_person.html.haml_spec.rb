c455cd71556b2c29e905b521e17fc85c7485952a
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2016-01-08 14:01:38 +0000

    Send analytics events for clicks on search results
    Set data-event-category and data-event-action attributes on search
    result links.

--- spec/views/people/_person.html.haml_spec.rb
+++ spec/views/people/_person.html.haml_spec.rb
@@ -11,12 +11,28 @@ RSpec.describe "rendering locals in a partial" do
     ]
   end
 
-  it "sets data-virtual-pageview correctly" do
+  before do
     render partial: 'people/person', collection: people, locals: { edit_link: false }
+  end
 
+  it "sets data-virtual-pageview correctly" do
     expect(rendered).to have_selector('[data-virtual-pageview="/top-3-search-result"]', text: people[0].name)
     expect(rendered).to have_selector('[data-virtual-pageview="/top-3-search-result"]', text: people[1].name)
     expect(rendered).to have_selector('[data-virtual-pageview="/top-3-search-result"]', text: people[2].name)
     expect(rendered).to have_selector('[data-virtual-pageview="/below-top-3-search-result"]', text: people[3].name)
+  end
+
+  it "sets data-event-category correctly" do
+    expect(rendered).to have_selector('[data-event-category="Search result"]', text: people[0].name)
+    expect(rendered).to have_selector('[data-event-category="Search result"]', text: people[1].name)
+    expect(rendered).to have_selector('[data-event-category="Search result"]', text: people[2].name)
+    expect(rendered).to have_selector('[data-event-category="Search result"]', text: people[3].name)
+  end
+
+  it "sets data-event-action correctly" do
+    expect(rendered).to have_selector('[data-event-action="Click result 1"]', text: people[0].name)
+    expect(rendered).to have_selector('[data-event-action="Click result 2"]', text: people[1].name)
+    expect(rendered).to have_selector('[data-event-action="Click result 3"]', text: people[2].name)
+    expect(rendered).to have_selector('[data-event-action="Click result 4"]', text: people[3].name)
   end
 end
