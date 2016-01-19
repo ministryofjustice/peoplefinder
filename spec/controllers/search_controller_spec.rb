706f4dcc5c8027b087cba1d304bad7706a7b085e
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2016-01-19 09:36:16 +0000

    Do team search in search controller and assign results
    Requirement is to show the exact name match team results
    in the search results view.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -3,26 +3,35 @@ require 'rails_helper'
 RSpec.describe SearchController, type: :controller do
   include PermittedDomainHelper
 
-  let(:search) { double(PersonSearch, perform_search: search_result) }
+  let(:person_search) { double(PersonSearch, perform_search: people_results) }
+  let(:group_search)  { double(GroupSearch, perform_search: team_results) }
 
-  let(:search_result) { double('Elasticsearch result') }
+  let(:people_results) { double('People results') }
+  let(:team_results)   { double('Team results') }
 
   before do
     mock_logged_in_user
-    allow(PersonSearch).to receive(:new).and_return(search)
+    allow(PersonSearch).to receive(:new).and_return(person_search)
+    allow(GroupSearch).to receive(:new).and_return(group_search)
   end
 
   context 'with valid UTF-8' do
     let(:query) { 'válïd ☺' }
 
     it 'searches for the query' do
-      expect(PersonSearch).to receive(:new).with(query).and_return(search)
+      expect(GroupSearch).to receive(:new).with(query).and_return(group_search)
+      expect(PersonSearch).to receive(:new).with(query).and_return(person_search)
       get :index, query: query
     end
 
-    it 'assigns the search result to @people' do
+    it 'assigns people search result to @people' do
       get :index, query: query
-      expect(assigns(:people)).to eq(search_result)
+      expect(assigns(:people)).to eq(people_results)
+    end
+
+    it 'assigns team search result to @teams' do
+      get :index, query: query
+      expect(assigns(:teams)).to eq(team_results)
     end
 
     it 'assigns the query to @query' do
@@ -35,13 +44,19 @@ RSpec.describe SearchController, type: :controller do
     let(:query) { "\x99" }
 
     it 'searches for an empty string' do
-      expect(PersonSearch).to receive(:new).with('').and_return(search)
+      expect(GroupSearch).to receive(:new).with('').and_return(group_search)
+      expect(PersonSearch).to receive(:new).with('').and_return(person_search)
       get :index, query: query
     end
 
     it 'assigns the search result to @people' do
       get :index, query: query
-      expect(assigns(:people)).to eq(search_result)
+      expect(assigns(:people)).to eq(people_results)
+    end
+
+    it 'assigns team search result to @teams' do
+      get :index, query: query
+      expect(assigns(:teams)).to eq(team_results)
     end
 
     it 'assigns an empty string to @query' do
