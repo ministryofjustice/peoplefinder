c8d4ac312f26960a3dd9afeca5b41c989d2e3bc4
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2016-01-19 15:45:49 +0000

    Return teams with partial matches on team name
    Return teams where the team name contains all of the words
    in the query string, ignoring case and punctuation.

--- app/services/group_search.rb
+++ app/services/group_search.rb
@@ -6,7 +6,26 @@ class GroupSearch
   def perform_search
     return [] if @query.blank?
 
+    (exact_matches + partial_matches).uniq
+  end
+
+  private
+
+  def words query
+    query.gsub(/\W/, ' ').split.select { |x| x.length > 1 }
+  end
+
+  def exact_matches
     Group.where(name: @query).to_a
+  end
+
+  def partial_matches
+    words = words(@query)
+    search = Group
+    words.each do |word|
+      search = search.where('name ILIKE ?', "%#{word}%")
+    end
+    search
   end
 
 end
--- spec/services/group_search_spec.rb
+++ spec/services/group_search_spec.rb
@@ -3,19 +3,31 @@ require 'rails_helper'
 RSpec.describe GroupSearch, elastic: true do
   let!(:department) { create(:department, name: 'Department name') }
   let!(:team) { create(:group, name: 'Team name', parent: department) }
+  let!(:civil_families_tribunal) { create(:group, name: 'Civil, Families & Tribunal', parent: department) }
+
   let!(:another_department) { create(:department, name: 'Another department name') }
-  let!(:team) { create(:group, name: 'Team name', parent: another_department) }
+  let!(:another_team) { create(:group, name: 'Team name', parent: another_department) }
 
   it 'returns empty array when query is blank' do
-    expect(described_class.new('').perform_search).to eq []
+    expect(search('')).to eq []
   end
 
   it 'returns matches when query is exact match for group name' do
-    expect(described_class.new('Team name').perform_search).to eq [team]
+    expect(search('Team name')).to eq [team, another_team]
   end
 
   it 'returns empty array when query is not exact match for group name' do
-    expect(described_class.new('Team').perform_search).to eq []
+    expect(search('Team number one')).to eq []
+  end
+
+  it 'returns matches when all words in query are in a group name' do
+    expect(search('civil')).to eq [civil_families_tribunal]
+    expect(search('civil families')).to eq [civil_families_tribunal]
+    expect(search('civil tribunal')).to eq [civil_families_tribunal]
+  end
+
+  def search query
+    described_class.new(query).perform_search
   end
 
 end
