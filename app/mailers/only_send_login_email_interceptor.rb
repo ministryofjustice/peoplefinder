29695a24efc300e9875c781c8badefe921cd6df9
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2016-02-04 17:25:49 +0000

    Add subject prefix on staging login emails

--- app/mailers/only_send_login_email_interceptor.rb
+++ app/mailers/only_send_login_email_interceptor.rb
@@ -6,13 +6,18 @@ class OnlySendLoginEmailInterceptor
   attr_reader :login_email_subject
 
   def initialize recipient_string, subject_prefix:, login_email_subject:
+    @subject_prefix = subject_prefix
     @interceptor = RecipientInterceptor.new(recipient_string,
       subject_prefix: subject_prefix)
     @login_email_subject = login_email_subject
   end
 
   def delivering_email message
-    @interceptor.delivering_email(message) unless login_email? message
+    if login_email? message
+      message.subject = "#{@subject_prefix} #{message.subject}" if @subject_prefix
+    else
+      @interceptor.delivering_email(message) unless login_email? message
+    end
   end
 
   private
