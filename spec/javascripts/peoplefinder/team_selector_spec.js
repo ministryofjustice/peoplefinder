16bba011101ee12581568ddd76f7f8438a16c63c
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2016-02-16 16:47:38 +0000

    Jasmine tests

--- spec/javascripts/peoplefinder/team_selector_spec.js
+++ spec/javascripts/peoplefinder/team_selector_spec.js
@@ -8,7 +8,7 @@ describe("Team Selector", function() {
       <div class="team visible"> \
         <h3><input type="radio" value="1" name="person[memberships_attributes][0][group_id]" id="person_memberships_attributes_0_group_id_1" /><a class="team-link" href="/teams/1" title="Ministry of Justice">Ministry of Justice</a></h3> \
         <ul> \
-          <li class="has-subteams expanded"> \
+          <li class="has-subteams"> \
             <p> \
               <input type="radio" value="2" name="person[memberships_attributes][0][group_id]" id="person_memberships_attributes_0_group_id_2" /> \
               <a id="digitalLink" class="subteam-link" href="#" title="Digital"> \
@@ -16,7 +16,7 @@ describe("Team Selector", function() {
                 <span class="subteam-count">1 sub-team</span> \
               </a> \
             </p> \
-            <div class="team visible"> \
+            <div class="team visible selected"> \
               <a class="team-back" href="#">Back</a> \
               <h3 class=""> \
                 <input type="radio" value="2" name="person[memberships_attributes][0][group_id]" id="person_memberships_attributes_0_group_id_2"> \
@@ -53,59 +53,78 @@ describe("Team Selector", function() {
     ts.initEvents();
   });
 
-  describe("Get the team name for an input", function(){
-    it("should be 'Technology'", function() {
-      expect(ts.getTeamName(input)).toBe('Technology');
+  describe("Basic functions", function(){
+    it("should remove 'selected' class from all .team elements", function() {
+      ts.removeSelected();
+      expect(obj.find('.selected').length).toEqual(0);
+    });
+    it("should get the last '.visible' element", function(){
+      var el = ts.getLastVisible().html(),
+        last = obj.find('.visible').last().html();
+      expect(el).toEqual(last);
+    });
+    describe("should set '.exapnded' class", function(){
+      beforeEach(function(){
+        ts.setExpanded();
+      });
+      setTimeout(function(){
+        it("should just do it", function(){
+          expect(obj.find('.expanded').length).toEqual(1);
+          expect(obj.find('.org-browser > .team > li').hasClass('expanded')).toBe(true);
+        });
+      }, 500);
+    });
+    describe("Get and set the current team name", function(){
+      it("should get the team name 'Technology'", function() {
+        expect(ts.getTeamName(input)).toBe('Technology');
+      });
+      it("should set the team name to 'Digital'", function() {
+        ts.setTeamName('Digital');
+        expect(teamName.text()).toBe('Digital team');
+      });
     });
   });
 
 
-  describe("Click on team with subteams", function(){
+  describe("Selecting a subteam", function(){
     beforeEach(function(){
       obj.find('#digitalLink').trigger('click');
     });
     it("should select the Digital radio button", function(){
       expect(orgBrowser.find('input:checked').val()).toEqual('2');
     });
-    it("should set the team name to 'Digital team'", function() {
-      expect(teamName.text()).toEqual('Digital team');
-    });
-  });
-
-  describe("Select a subteam", function(){
-    obj.find('#digitalLink').trigger('click');     
     setTimeout(function(){
       obj.find('#techLink').trigger('click');
       it("should select the Technology radio button", function(){
         expect(orgBrowser.find('input:checked').val()).toEqual('3');
-      });
-      it("should set the team name to 'Technology team'", function() {
-        expect(teamName.text()).toEqual('Technology team');
       });
     }, 500);
   });
 
   describe("Create a new team", function(){
     var teamData = {id:4,name:'Content',parentId:2,parentName:'Digital'};
+    describe("Clicking the add team link", function(){
+      beforeEach(function(){
+          obj.find('#addTeamLink').trigger('click');
+      });
+      setTimeout(function(){
+        it("should display the new team input field", function(){
+          expect(obj.find('.new-team').is(':visible')).toBe(true);
+        });
+      }, 100);
+    });
 
     describe("Clicking the add team link", function(){
       beforeEach(function(){
-        obj.find('#addTeamLink').trigger('click');
+        ts.addTeamToList(teamData);
       });
-      // it("should display the new team input field", function(){
-      //   expect(obj.find('.new-team').is(':visible')).toBe(true);
-      // });
-    });
 
-    beforeEach(function(){
-      ts.addTeamToList(teamData);
+      setTimeout(function(){
+        it("should create a new radio input", function(){
+          expect(orgBrowser.find('input[value="4"]')).toExist();
+        });
+      }, 500);
     });
-
-    setTimeout(function(){
-      it("should create a new radio input", function(){
-        expect(orgBrowser.find('input[value="4"]')).toExist();
-      });
-    }, 500);
 
   });
 
