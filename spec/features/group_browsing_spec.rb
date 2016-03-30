670678ee65e670c41c1f0d76d2013b42822a5ceb
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2016-03-30 11:41:20 +0100

    Add testing for organogram

--- spec/features/group_browsing_spec.rb
+++ spec/features/group_browsing_spec.rb
@@ -55,6 +55,7 @@ feature 'Group browsing' do
     expect(page).not_to have_text("Teams within #{current_group.name}")
     expect(page).not_to have_link("View all 0 people in #{current_group.name}")
     expect(page).not_to have_link("View 0 people not assigned to a sub-team")
+    expect(page).not_to have_link("View organogram")
   end
 
   context 'A team with people and subteams with people' do
@@ -70,6 +71,7 @@ feature 'Group browsing' do
       visit group_path(department)
       expect(page).not_to have_link("View all 7 people in #{department.name}")
       expect(page).to have_link("View 1 person not assigned to a sub-team")
+      expect(page).to have_link("View organogram")
     end
 
     scenario 'viewing text on page' do
@@ -107,6 +109,22 @@ feature 'Group browsing' do
       end
 
       expect(page).to have_text("People in #{team.name} not assigned to a sub-team")
+      names.each do |name|
+        expect(page).to have_link(name.join(' '))
+      end
+    end
+
+    scenario 'following the view organogram link' do
+      visit group_path(team)
+      click_link("View organogram")
+
+      expect(page).to have_title("#{team.name} Organogram - #{app_title}")
+      within('.breadcrumbs') do
+        expect(page).to have_link(team.name)
+        expect(page).to have_text('Organogram')
+      end
+
+      expect(page).to have_text("#{team.name} team organogram")
       names.each do |name|
         expect(page).to have_link(name.join(' '))
       end
