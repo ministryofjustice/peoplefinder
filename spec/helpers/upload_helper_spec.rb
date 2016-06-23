a4c184c20c9217e79629aa402d0e1c087ae1e92e
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-06-23 15:10:29 +0100

    separate and style csv uploader hint into partial
    The help text html was in a translator but was becoming
    too verbose. additionally partialised so as to use helpers.

---
+++ spec/helpers/upload_helper_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe UploadHelper, type: :helper do
+  describe 'required_headers' do
+    let(:html) do
+      '<code>given_name</code> ' \
+      '<code>surname</code> ' \
+      '<code>email</code>'
+    end
+    it "returns expected html wrapped list of optional columns" do
+      expect(required_headers).to eql(html)
+    end
+  end
+
+  describe 'optional_headers' do
+    let(:html) do
+      '<code>primary_phone_number</code> ' \
+      '<code>building</code> ' \
+      '<code>location_in_building</code> ' \
+      '<code>city</code>'
+    end
+    it "returns expected html wrapped list of optional columns" do
+      expect(optional_headers).to eql(html)
+    end
+  end
+end
