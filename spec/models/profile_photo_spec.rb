b6fa3f38249d3d1421085bd6f556bf407133ede9
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-07-05 15:43:47 +0100

    validate file size
    This is currently not enforced and can
    cause delays and potential timeout issues
    (14 sec max).

--- spec/models/profile_photo_spec.rb
+++ spec/models/profile_photo_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ProfilePhoto, type: :model do
-  subject do
-    create(:profile_photo)
+  subject { create(:profile_photo) }
+
+  it 'has one person' do
+    person_association = described_class.reflect_on_association(:person).macro
+    expect(person_association).to eql :has_one
   end
 
   it 'crops the image' do
@@ -13,4 +16,17 @@ RSpec.describe ProfilePhoto, type: :model do
     expect(subject.crop_w).to eq(3)
     expect(subject.crop_h).to eq(4)
   end
+
+  describe 'validations' do
+    subject { build(:profile_photo) }
+
+    it 'validates file size not to exceed 3M' do
+      allow(subject.image).to receive(:size).and_return 3.001.megabytes
+      expect(subject).to_not be_valid
+      expect(subject.errors[:image].first).to match(/file size.*too large/)
+      allow(subject.image).to receive(:size).and_return 3.megabytes
+      expect(subject).to be_valid
+    end
+  end
+
 end
