02a5f5a88de3f7a04bacef73127af4675d9ba929
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-08-10 20:39:14 +0100

    tweak search result class spec

--- spec/services/search_results_spec.rb
+++ spec/services/search_results_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe SearchResults, type: :service do
-
   it { is_expected.to respond_to(:set) }
   it { is_expected.to respond_to(:set=) }
   it { is_expected.to respond_to(:contains_exact_match) }
@@ -17,19 +16,18 @@ RSpec.describe SearchResults, type: :service do
   end
 
   describe '#clear' do
-
-    subject { described_class.new([1, 2], false) }
+    subject { described_class.new([1, 2], true) }
 
     it 'returns self' do
       expect(subject.clear).to be subject
     end
 
-    it 'clears state' do
+    it 'clears all result state' do
       expect(subject.set).to_not be_empty
+      expect(subject.contains_exact_match).to eq true
       subject.clear
       expect(subject.set).to be_empty
       expect(subject.contains_exact_match).to eq false
     end
   end
-
 end
