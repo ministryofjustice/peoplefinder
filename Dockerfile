d21e30f2e05ba4cebd8bf461d46aa614715b4df8
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-08-16 12:27:14 +0100

    add trace  and explict assets to env file

--- Dockerfile
+++ Dockerfile
@@ -66,8 +66,7 @@ COPY app/assets/ /usr/src/app/app/assets/
 COPY vendor/assets/ /usr/src/app/vendor/assets/
 COPY Rakefile /usr/src/app/
 
-RUN bundle exec rake assets:precompile RAILS_ENV=assets SUPPORT_EMAIL=''
-
+RUN bundle exec rake assets:precompile RAILS_ENV=assets SUPPORT_EMAIL='' --verbose
 COPY . /usr/src/app
 
 ENTRYPOINT ["./run.sh"]
--- config/environments/assets.rb
+++ config/environments/assets.rb
@@ -6,4 +6,12 @@ Rails.application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0.6'
+
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
+  # add any peoplefinder subdirectories to assets pipeline
+  Dir.glob("#{Rails.root}/app/assets/**/").each do |path|
+    config.assets.paths << path if path =~ /peoplefinder/
+  end
+
 end
