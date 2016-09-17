bdc1c1d66ed5177320ac3ea1b9ecdf2b07f28df1
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-09-17 22:23:30 +0100

    rubo facistise the changes

--- app/models/concerns/completion.rb
+++ app/models/concerns/completion.rb
@@ -24,7 +24,7 @@ module Concerns::Completion
   included do
     def self.inadequate_profiles
       where(inadequate_profiles_sql).
-      order(:email)
+        order(:email)
     end
 
     # This is not scalable
@@ -58,8 +58,8 @@ module Concerns::Completion
     end
 
     # scalable
-    def self.average_completion_score id=nil
-      results = ActiveRecord::Base.connection.execute(average_completion_sql id)
+    def self.average_completion_score(id = nil)
+      results = ActiveRecord::Base.connection.execute(average_completion_sql(id))
       results.first[avg_alias].to_f.round
     end
 
@@ -97,17 +97,18 @@ module Concerns::Completion
       profile_photo_missing = "( COALESCE(cast(profile_photo_id AS text), '') = '' AND " \
         "COALESCE(cast(image AS text), '') = '' )"
       sql += " OR #{profile_photo_missing}"
+      sql
     end
 
     def self.avg_alias
       'average_completion_score'
     end
 
-    def self.average_completion_sql id=nil
+    def self.average_completion_sql(id = nil)
       sql = "SELECT AVG(( \n"
       sql += completion_score_calculation
       sql += ") * 100)::numeric(5,2) AS #{avg_alias}"
-      sql += " FROM \"people\""
+      sql += ' FROM "people"'
       if id.present?
         ids = *id
         sql += " WHERE \"people\".id IN (#{ids.join(',')})"
@@ -123,14 +124,20 @@ module Concerns::Completion
     # NOTE: - groups "field" requires a join and therefore needs separate handling for scalability
     #       - photo "field" requires checking for legacy images as well
     def self.completion_score_sum
-       sum_sql = COMPLETION_FIELDS.each_with_object(String.new) do |field, string|
+      sum_sql = COMPLETION_FIELDS.each_with_object('') do |field, string|
         if field == :groups
-          string.concat(" + CASE WHEN (SELECT 1 WHERE EXISTS (SELECT 1 FROM memberships m WHERE m.person_id = people.id)) IS NOT NULL THEN 1 ELSE 0 END \n")
+          string.concat(" + CASE WHEN (SELECT 1 \n" \
+                                      "WHERE EXISTS (SELECT 1 \n" \
+                                                      "FROM memberships m \n" \
+                                                      "WHERE m.person_id = people.id)) IS NOT NULL \n" \
+                                  "THEN 1 \n" \
+                                "ELSE 0 \n" \
+                            "END \n")
         elsif field == :profile_photo_present?
-          string.concat(" + (CASE WHEN length(profile_photo_id::varchar) > 0 THEN 1 \n" +
-                                "WHEN length(image) > 0 THEN 1 \n" +
-                                "ELSE 0 \n" +
-                            "END)")
+          string.concat(" + (CASE WHEN length(profile_photo_id::varchar) > 0 THEN 1 \n" \
+                                "WHEN length(image) > 0 THEN 1 \n" \
+                                "ELSE 0 \n" \
+                            "END)\n")
         else
           string.concat(" + (CASE WHEN length(#{field}::varchar) > 0 THEN 1 ELSE 0 END) \n")
         end
@@ -139,30 +146,29 @@ module Concerns::Completion
       sum_sql[2..-1]
     end
 
-    def self.bucket_case_statement alias_name=avg_alias
+    def self.bucket_case_statement(alias_name = avg_alias)
       BUCKETS.inject('CASE') do |memo, range|
         memo + "\nWHEN #{alias_name} BETWEEN #{range.begin} AND #{range.end} THEN \'[#{range.begin},#{range.end}]\'"
       end + "\nEND AS bucket\n"
     end
 
     def self.bucketed_completion_score_sql
-      "SELECT people_count," +
-      bucket_case_statement(avg_alias) +
-      "FROM (
-        SELECT count(id) AS people_count, \n" +
-        "(#{completion_score_calculation} * 100)::numeric(5,2) AS #{avg_alias} \n" +
-        " FROM \"people\" GROUP BY (#{avg_alias}) \n" +
-      ") AS buckets"
+      "SELECT people_count,\n" +
+        bucket_case_statement(avg_alias) +
+        "FROM (\n" \
+          "SELECT count(id) AS people_count, \n" \
+          "(#{completion_score_calculation} * 100)::numeric(5,2) AS #{avg_alias} \n" \
+          " FROM \"people\" GROUP BY (#{avg_alias}) \n" \
+        ') AS buckets'
     end
 
     def self.parse_bucketed_results results
-      results = results.inject(Hash.new) { |memo, tuple| memo.merge( tuple['bucket'] => tuple['people_count'].to_i) }
+      results = results.inject({}) { |memo, tuple| memo.merge(tuple['bucket'] => tuple['people_count'].to_i) }
       default_bucket_scores.merge results
     end
 
     def self.default_bucket_scores
       Hash[Person::BUCKETS.map { |r| ["[#{r.begin},#{r.end}]", 0] }]
     end
-
   end
 end
