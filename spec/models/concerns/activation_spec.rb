d1961390c6e26e984962cfd6044a2f78fd12c741
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-09-22 22:42:46 +0100

    amend activation metrics to ensure they are scalable
    Stops acquisition and activation metrics raising 5xx errors. This is almost certainly a result of the non-scalable combination of AR sql and ruby looping. Tested locally using:
    
    ```
    puts Benchmark.measure { Person.activated_percentage(from: Date.today-365 }
    ```
      - [X] refactor the concerns/mixins
      - [X] ensure scalability
      - [X] spec

--- spec/models/concerns/activation_spec.rb
+++ spec/models/concerns/activation_spec.rb
@@ -22,6 +22,12 @@ RSpec.describe Concerns::Activation do
   end
 
   context '.activated_percentage' do
+
+    it 'uses .completion_score_calculation for scalablility' do
+      expect(Person).to receive(:completion_score_calculation).and_return(0.81)
+      Person.activated_percentage
+    end
+
     it 'returns 0 when no profiles' do
       expect(Person.activated_percentage).to eq(0)
     end
@@ -41,13 +47,13 @@ RSpec.describe Concerns::Activation do
       expect(Person.activated_percentage).to eq(100)
     end
 
-    it 'returns 50 when two people who have logged in, one with completeness > 80%, one with completeness > 80%' do
+    it 'returns 50 when two people who have logged in, one with completeness > 80%, one with completeness < 80%' do
       create(:person, completed_attributes.merge(login_count: 1))
       create(:person, login_count: 1)
       expect(Person.activated_percentage).to eq(50)
     end
 
-    it 'returns 67 when 3 people who have logged in, two with completeness > 80%, one with completeness > 80%' do
+    it 'returns 67 when 3 people who have logged in, two with completeness > 80%, one with completeness < 80%' do
       create(:person, completed_attributes.merge(login_count: 1))
       create(:person, completed_attributes.merge(email: 'test2@digital.justice.gov.uk', login_count: 1))
       create(:person, login_count: 1)
