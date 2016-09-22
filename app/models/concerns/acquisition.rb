08069bd5bd87fdbdc17df72afff9688ba6ce4ac0
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-09-22 22:55:10 +0100

    refactor

--- app/models/concerns/acquisition.rb
+++ app/models/concerns/acquisition.rb
@@ -5,8 +5,7 @@ module Concerns::Acquisition
     # % of people created between specified dates (from,before)
     # and who have logged in at least once
     def acquired_percentage from: nil, before: nil
-      people_count = Person.count
-      (acquired_people(from: from, before: before).count.to_f / people_count * 100).round(0)
+      (acquired_people(from: from, before: before).count.to_f / count * 100).round(0)
     rescue FloatDomainError, ZeroDivisionError
       0
     end
@@ -14,7 +13,7 @@ module Concerns::Acquisition
     private
 
     def acquired_people from: nil, before: nil
-      acquired = Person.logged_in_at_least_once
+      acquired = logged_in_at_least_once
       acquired = acquired.where('created_at >= ?', from) if from
       acquired = acquired.where('created_at < ?', before) if before
       acquired
