5b4cea0ba359d901903c300294ea69b30e2e645e
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-10-31 09:56:47 +0000

    add pageview and event helper for team edit click action

--- spec/helpers/analytics_helper_spec.rb
+++ spec/helpers/analytics_helper_spec.rb
@@ -28,4 +28,14 @@ RSpec.describe AnalyticsHelper, type: :helper do
       expect(edit_profile_analytics_attributes(3)).to include "event-category": 'Edit profile click', "event-action": "Click edit person 3"
     end
   end
+
+  describe '#edit_team_analytics_attributes' do
+    it 'returns JSON including virtual pageview data expected by GA' do
+      expect(edit_team_analytics_attributes(1)).to include "virtual-pageview": "/teams/edit-click"
+    end
+
+    it 'returns JSON event data for GA' do
+      expect(edit_team_analytics_attributes(3)).to include "event-category": 'Edit team click', "event-action": "Click edit team 3"
+    end
+  end
 end
