13d865bcc553b5c363b6292f6e98ac972359762e
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-11-14 10:32:37 +0000

    prevent scheduling of jobs in dev and staging hosted environments

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,31 +1,33 @@
-set :output, '/var/log/cron_log.log'
+if ENV['ENV'] == 'production'
+  set :output, '/var/log/cron_log.log'
 
-job_type :rails_script, "cd /usr/src/app && ./rails_runner.sh ':task' :output"
+  job_type :rails_script, "cd /usr/src/app && ./rails_runner.sh ':task' :output"
 
-every :weekday, at: '8am' do
-  rails_script 'NeverLoggedInNotifier.send_reminders'
-end
+  every :weekday, at: '8am' do
+    rails_script 'NeverLoggedInNotifier.send_reminders'
+  end
 
-every :weekday, at: '8:10am' do
-  rails_script 'TeamDescriptionNotifier.send_reminders'
-end
+  every :weekday, at: '8:10am' do
+    rails_script 'TeamDescriptionNotifier.send_reminders'
+  end
 
-every :weekday, at: '8:15am' do
-  rails_script 'PersonUpdateNotifier.send_reminders'
-end
+  every :weekday, at: '8:15am' do
+    rails_script 'PersonUpdateNotifier.send_reminders'
+  end
 
-every :weekday, at: '8:00pm' do
-  rails_script 'GeckoboardPublisher::PhotoProfilesReport.new.publish!'
-end
+  every :weekday, at: '8:00pm' do
+    rails_script 'GeckoboardPublisher::PhotoProfilesReport.new.publish!'
+  end
 
-every :weekday, at: '8:10pm' do
-  rails_script 'GeckoboardPublisher::ProfilesPercentageReport.new.publish!'
-end
+  every :weekday, at: '8:10pm' do
+    rails_script 'GeckoboardPublisher::ProfilesPercentageReport.new.publish!'
+  end
 
-every :weekday, at: '8:20pm' do
-  rails_script 'GeckoboardPublisher::TotalProfilesReport.new.publish!'
-end
+  every :weekday, at: '8:20pm' do
+    rails_script 'GeckoboardPublisher::TotalProfilesReport.new.publish!'
+  end
 
-every :weekday, at: '8:30pm' do
-  rails_script 'GeckoboardPublisher::ProfilesChangedReport.new.publish!'
+  every :weekday, at: '8:30pm' do
+    rails_script 'GeckoboardPublisher::ProfilesChangedReport.new.publish!'
+  end
 end
