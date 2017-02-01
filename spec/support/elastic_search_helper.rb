15ada3c67769835fc290f9ca36dcb63b4321ad76
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-02-01 16:53:38 +0000

    add elasticsearch test helpers

--- spec/support/elastic_search_helper.rb
+++ spec/support/elastic_search_helper.rb
@@ -1,18 +1,19 @@
 module SpecSupport
   module ElasticSearchHelper
 
-    def hit_count
+    def total_document_count
       Person.search('*').results.total
     end
 
-    def all_documents
-      Person.search('*').records.map_with_hit do |rec, hit|
-        { name: rec.name, score: hit._score }
+    def all_documents(limit: 100)
+      response = Person.search(match_all(limit: limit))
+      response.records.map_with_hit do |rec, hit|
+        { name: rec.name, hit: hit }
       end
     end
 
-    def print_all_documents
-      p = IO.popen("curl -i -XGET 'localhost:9200/#{Person.index_name}/person/_search?pretty'")
+    def print_all_documents(limit: 100)
+      p = IO.popen("curl -i -XGET 'localhost:9200/#{Person.index_name}/person/_search?size=#{limit}&pretty'")
       puts p.readlines
       p.close
     end
@@ -20,5 +21,17 @@ module SpecSupport
     def index_health
       Person.__elasticsearch__.client.cluster.health
     end
+
+    private
+
+    def match_all(limit: 100)
+      {
+        size: limit,
+        query: {
+          match_all: {}
+        }
+      }
+    end
+
   end
 end
