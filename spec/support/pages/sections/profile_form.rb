fcbd2c040fe83bf2143533f1394ad7abc73f8c79
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-03-17 17:26:17 +0000

    apply translations to person form fields
    part of fix for person_maintenance_spec

--- spec/support/pages/sections/profile_form.rb
+++ spec/support/pages/sections/profile_form.rb
@@ -4,15 +4,17 @@ module Pages
       element :global_error, '.alert.alert-error'
 
       element :given_name, '#person_given_name'
-      element :given_name_error, :xpath, '//label[@for="person_given_name"]/*[@class="error"]'
+      # element :given_name_error, :xpath, '//label[@for="person_given_name"]/*[@class="error"]'
+      element :given_name_error, "a[href='#error_person_given_name']"
 
       element :surname, '#person_surname'
-      element :surname_error, :xpath, '//label[@for="person_surname"]/*[@class="error"]'
+      # element :surname_error, :xpath, '//label[@for="person_surname"]/*[@class="error"]'
+      element :surname_error, "a[href='#error_person_surname']"
 
       element :email, '#person_email'
-      element :email_error, :xpath, '//label[@for="person_email"]/*[@class="error"]'
+      element :email_error, "a[href='#error_person_email']"
 
-      element :save, '.save-cancel-actions input[type=submit]'
+      element :save, "input[type=submit][value='Save']"
     end
   end
 end
