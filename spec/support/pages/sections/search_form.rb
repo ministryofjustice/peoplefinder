e3dd6282abfe479366a15b6040cbe378685490c3
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-03-17 17:14:34 +0000

    fix home page feature test - again

--- spec/support/pages/sections/search_form.rb
+++ spec/support/pages/sections/search_form.rb
@@ -1,7 +1,7 @@
 module Pages
   module Sections
     class SearchForm < SitePrism::Section
-      element :search_field, '.search-box'
+      element :search_field, "input[type='text'][id='query']"
       element :search_button, "input[type='submit'][value='Submit search'][class='button']"
     end
   end
