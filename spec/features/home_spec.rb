0c77d481b9aa19817a02ccd1d144ada6713dc937
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-03-17 10:32:42 +0000

    add home page siteprism spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,29 +3,46 @@ require 'rails_helper'
 feature 'Home page' do
   include PermittedDomainHelper
 
+  let(:home_page) { Pages::Home.new }
+
+  before do
+    create(:department)
+  end
+
+  context 'page structure' do
+    before do
+      mock_readonly_user
+      visit '/'
+    end
+
+    it 'is all there' do
+      expect(home_page).to have_page_title
+      expect(home_page).to have_search_form
+      expect(home_page.page_title).to have_text('Welcome to People Finder')
+    end
+  end
+
   context 'for a regular user' do
     before do
-      create(:department)
       omni_auth_log_in_as 'test.user@digital.justice.gov.uk'
       visit '/'
     end
 
-    scenario 'Viewing the page' do
-      within('h1') { expect(page).to have_text('Welcome to People Finder') }
-      expect(page).to have_css('input#query')
+    scenario 'Can view the page' do
+      expect(home_page).to be_displayed
+      expect(home_page).to be_all_there
     end
   end
 
   context 'for the readonly user' do
     before do
-      create(:department)
       mock_readonly_user
       visit '/'
     end
 
-    scenario 'Viewing the page' do
-      within('h1') { expect(page).to have_text('Welcome to People Finder') }
-      expect(page).to have_css('input#query')
+    scenario 'Can view the page' do
+      expect(home_page).to be_displayed
+      expect(home_page).to be_all_there
     end
   end
 end
