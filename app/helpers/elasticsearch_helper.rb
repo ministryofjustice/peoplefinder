d4fd937b959c916cc34f345388171bd1bf16469d
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-03-24 13:14:40 +0000

    remove xss attack vector
    elastic search highlighting requires an html_safe
    on the es rendered html. This meant that js script
    tags could be rendered unescaped.

---
+++ app/helpers/elasticsearch_helper.rb
@@ -0,0 +1,25 @@
+module ElasticsearchHelper
+
+  HIGHLIGHTER_TAGS = (PersonSearch::PRE_TAGS + PersonSearch::POST_TAGS).freeze
+
+  def es_highlighter hit, person, attribute
+    hit.try(:highlight).try(attribute) ? sanitize_highlighter(hit, person, attribute) : person.__send__(attribute)
+  end
+
+  private
+
+  def sanitize_highlighter hit, person, attribute
+    unsanitized = hit.highlight.__send__(attribute).join.html_safe
+    unhighlighted = strip_highlighting!(hit.highlight.__send__(attribute).join.html_safe)
+    sanitized = sanitize(person.__send__(attribute))
+
+    unhighlighted == sanitized ? unsanitized : person.__send__(attribute)
+  end
+
+  def strip_highlighting! html
+    HIGHLIGHTER_TAGS.each_with_object(html) do |tag, memo|
+      memo.gsub!(tag, '')
+    end
+  end
+
+end
