411eb6443f98b986f88105abf95715ea9c204c48
David Plews (ClearGif) <freelance@cleargif.co.uk>
2017-03-27 15:36:28 +0100

    Edit profile: Main layout + GDS form
    https://dsdmoj.atlassian.net/browse/PFT-268

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,27 @@ $(function() {
     $('#new_problem_report').slideToggle('slow');
   });
 });
+
+
+/* global $, document, teamSelector */
+
+$(function() {
+  $(document).on('click', '#add_membership', function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: this,
+      success: function(data) {
+        console.log('SUCCESS');
+        var el_to_add = $(data).html();
+        $('#memberships').append(el_to_add);
+        var team = new teamSelector(true, $('#memberships').find('.membership').last());
+        team.initEvents();
+      }
+    });
+  });
+
+  $(document).on('click', 'a.remove-new-membership', function(e) {
+    e.preventDefault();
+    $(this).parents('.membership').remove();
+  });
+});
