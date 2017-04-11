6ad72d4cd11959c6c8008db4476edab01033a93b
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-04-11 11:06:51 +0100

    fix more specs
    - [X] token_authentication
    - [X] people_controller
    - [X] login_flow_spec
    - [X] search_spec

---
+++ spec/support/pages/sections/person_confirm_search_results.rb
@@ -0,0 +1,26 @@
+module Pages
+  module Sections
+    class PersonConfirmSearchResultSection < SitePrism::Section
+      element :name_link, ".cb-person-name > a"
+      element :select_button, "input[type='submit'][value='Select']"
+    end
+  end
+end
+
+module Pages
+  module Sections
+    class PersonConfirmSearchResults < SitePrism::Section
+      sections :confirmation_results, Sections::PersonConfirmSearchResultSection, '.cb-confirm-search-result'
+
+      def name_links
+        confirmation_results.map do |sr|
+          sr.name_link['href']
+        end
+      end
+
+      def select_buttons
+        confirmation_results.map(&:select_button)
+      end
+    end
+  end
+end
