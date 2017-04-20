f9ad586503ab83921e367c8e1b55305583d88c0b
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-04-20 10:51:42 +0100

    move warning to translation

--- app/helpers/user_agent_helper.rb
+++ app/helpers/user_agent_helper.rb
@@ -2,7 +2,7 @@ module UserAgentHelper
   Browser = Struct.new(:browser, :version)
 
   SUPPORTED_BROWSERS = [
-      Browser.new('Safari', '6.0'),
+      Browser.new('Safari', '4.0'),
       Browser.new('Firefox', '19.0'),
       Browser.new('Internet Explorer', '8.0'),
       Browser.new('Chrome', '25.0'),
@@ -13,8 +13,8 @@ module UserAgentHelper
   end
 
   def supported_browser?
-    # ap "<<<<<<<<<<<< LINE #{__LINE__} #{user_agent.browser}:#{user_agent.version}>>>>>>>>>>>>>>"
     SUPPORTED_BROWSERS.any? do |browser|
+      # puts "<<<<<<<<<<<< LINE #{__LINE__} #{user_agent.browser}:#{user_agent.version} >= #{browser.browser}:#{browser.version}: #{user_agent >= browser}>>>>>>>>>>>>>>"
       user_agent >= browser
     end
   end
