6b3ad36d354196717df30ec92f29f632c6d41813
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-05-08 13:31:33 +0100

    add link to team icon
    search results have links on the people
    profiles but not on the teams.

--- spec/helpers/people_helper_spec.rb
+++ spec/helpers/people_helper_spec.rb
@@ -47,6 +47,10 @@ RSpec.describe PeopleHelper, type: :helper do
       expect(profile_image_tag(person, options)).to match(/.*href=\"\/people\/.*\".*/)
     end
 
+    it 'adds alternative text to the person profile by default' do
+      expect(profile_image_tag(person, options)).to match(/.*alt=\"Current photo of.*\".*/)
+    end
+
     it 'does not add a link to when option set' do
       expect(profile_image_tag(person, options.merge(link: false))).to_not match(/.*href=.*/)
     end
@@ -55,8 +59,15 @@ RSpec.describe PeopleHelper, type: :helper do
       expect(profile_image_tag(person, options)).to match(/.*profile_photo.*\/croppable.*/)
     end
 
-    it 'removes the version element from options hash' do
-      expect { profile_image_tag(person, options) }.to change { options.keys }.from([:class, :version]).to([:class, :link])
+    it 'adds a link_uri and alt_text to options hash and removes the version element' do
+      expect { profile_image_tag(person, options) }.to change { options.keys }.from([:class, :version]).to([:class, :link_uri, :alt_text])
+    end
+
+    it 'does not output internally used options' do
+      html = profile_image_tag(person, options)
+      expect(html).not_to match(/.*link=.*/)
+      expect(html).not_to match(/.*link_uri=.*/)
+      expect(html).not_to match(/.*alt_text=.*/)
     end
 
     it 'defaults to using the medium image version' do
@@ -67,6 +78,27 @@ RSpec.describe PeopleHelper, type: :helper do
     it 'fallsback to using medium_no_photo.png' do
       person.profile_photo_id = nil
       expect(profile_image_tag(person, options)).to match(/.*\/medium_no_photo.png.*/)
+    end
+  end
+
+  describe 'team_image_tag' do
+    let(:team) { create(:group) }
+    let(:options) { { class: 'my-class' } }
+
+    it 'adds a link to the team profile by default' do
+      expect(team_image_tag(team, options)).to match(/.*href=\"\/teams\/.*\".*/)
+    end
+
+    it 'does not add a link to when option set' do
+      expect(team_image_tag(team, options.merge(link: false))).to_not match(/.*href=.*/)
+    end
+
+    it 'uses the medium team image' do
+      expect(team_image_tag(team, options)).to match(/.*\/medium_team.*/)
+    end
+
+    it 'adds a link uri to options hash' do
+      expect { team_image_tag(team, options) }.to change { options.keys }.from([:class]).to([:class, :link_uri, :alt_text])
     end
   end
 
