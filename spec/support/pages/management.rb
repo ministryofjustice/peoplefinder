bd4a422849f6af237072c63a172035e6503ff8ee
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-06-20 14:42:05 +0100

    add access to audit trail from management screen

--- spec/support/pages/management.rb
+++ spec/support/pages/management.rb
@@ -5,5 +5,6 @@ module Pages
     element :generate_link, '.generate-link > a'
     element :download_link, '.download-link > a'
     element :bulk_uploader_link, '#bulk-uploader > a'
+    element :audit_trail_link, '#audit-trail > a'
   end
 end
