b23a418cf027a16d4b3b71c5d8b280621a4eae66
Toby Privett <toby@snaplab.co.uk>
2017-09-29 15:29:08 +0100

    Remove token authentication from the api

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -2,19 +2,5 @@ module Api
   class ApplicationController < ActionController::Base
 
     protect_from_forgery with: :exception
-    before_action :authenticate!
-
-    private
-
-    def authenticate!
-      token = Token.where(value: authorization_token).first
-      unless token
-        render json: { errors: 'Unauthorized' }, status: :unauthorized
-      end
-    end
-
-    def authorization_token
-      request.headers['AUTHORIZATION'] || params[:token]
-    end
   end
 end
