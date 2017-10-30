1c9b91eefed8799dd25ba6c8fb225a4bbdb2db2f
Toby Privett <toby@snaplab.co.uk>
2017-10-30 12:49:57 +0000

    Update the crontab script path for Paas

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ if ENV['ENV'] == 'production'
 
   set :output, standard: '/var/log/cron.log', error: '/var/log/cron_error.log'
 
-  job_type :rails_script, "cd /usr/src/app && ./rails_runner.sh ':task' :output"
+  job_type :rails_script, "cd /home/vcap/app && ./rails_runner.sh ':task' :output"
 
   every 5.minutes do
     rails_script 'NotificationSender.new.send!'
