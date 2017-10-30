1bde0c357457c1d17382d377963535ad85805076
Toby Privett <toby@snaplab.co.uk>
2017-10-30 12:57:42 +0000

    Implement database session clean up task

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,11 +3,15 @@ if ENV['ENV'] == 'production'
   set :output, standard: '/var/log/cron.log', error: '/var/log/cron_error.log'
 
   job_type :rails_script, "cd /home/vcap/app && ./rails_runner.sh ':task' :output"
+  job_type :rake_task, 'cd /home/vcap/app && :environment_variable=:environment bundle exec rake :task --silent :output'
 
   every 5.minutes do
     rails_script 'NotificationSender.new.send!'
   end
 
+  every 1.day, at: '4:30 am' do
+    rake 'db:sessions:trim'
+  end
 end
 
 if %w(dev demo staging).include? ENV['ENV']
