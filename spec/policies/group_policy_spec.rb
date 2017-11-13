619e03f7190b6d5024f1bd243b8d75e039ddb38c
Jordan Hatch <jordan@jordanh.net>
2017-11-13 16:33:56 +0000

    Update GroupPolicy to require admin role for changes

--- spec/policies/group_policy_spec.rb
+++ spec/policies/group_policy_spec.rb
@@ -9,6 +9,17 @@ RSpec.describe GroupPolicy, type: :policy do
     let(:user) { build_stubbed(:person) }
 
     it { is_expected.to permit_action(:show) }
+    it { is_expected.not_to permit_action(:edit) }
+    it { is_expected.not_to permit_action(:update) }
+    it { is_expected.not_to permit_action(:new) }
+    it { is_expected.not_to permit_action(:create) }
+    it { is_expected.not_to permit_action(:destroy) }
+  end
+
+  context 'for a super admin' do
+    let(:user) { build_stubbed(:super_admin) }
+
+    it { is_expected.to permit_action(:show) }
     it { is_expected.to permit_action(:edit) }
     it { is_expected.to permit_action(:update) }
     it { is_expected.to permit_action(:new) }
