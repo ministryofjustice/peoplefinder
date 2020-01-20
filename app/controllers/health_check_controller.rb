6e3e976d7571e8209e9c7f3f13c8c7a6bb1ec641
Javid Ali <javid.ali@digital.justice.gov.uk>
2020-01-20 12:36:37 +0000

    CT-2636 Restore health check to normal status

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -5,12 +5,12 @@ class HealthCheckController < ActionController::Base
   def index
     Rails.logger.silence do
       report = HealthCheckService.new.report
-      render json: report, status: '500'
-      # if report.status == '200'
-      #   render json: report
-      # else
-      #   render json: report, status: '500'
-      # end
+
+      if report.status == '200'
+        render json: report
+      else
+        render json: report, status: '500'
+      end
     end
   end
 end
