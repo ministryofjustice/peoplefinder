1a7b387eee6ce4aaf0d07da5ac08f61a3250e216
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2020-04-22 23:06:10 +0100

    Rubocop changes

--- config/initializers/action_view_patch.rb
+++ config/initializers/action_view_patch.rb
@@ -3,26 +3,23 @@
 # Monkey patch escape JavaScript method to prevent XSS vulnerability
 # https://github.com/ministryofjustice/peoplefinder/network/alert/Gemfile.lock/actionview/open
 
-ActionView::Helpers::JavaScriptHelper::JS_ESCAPE_MAP.merge!(
-  {
-    "`" => "\\`",
-    "$" => "\\$"
-  }
-)
-
+ActionView::Helpers::JavaScriptHelper::JS_ESCAPE_MAP['`'] = '\\`'
+ActionView::Helpers::JavaScriptHelper::JS_ESCAPE_MAP['$'] = '\\$'
+# rubocop:disable Rails/OutputSafety
 module ActionView::Helpers::JavaScriptHelper
   alias old_ej escape_javascript
   alias old_j j
 
   def escape_javascript(javascript)
     javascript = javascript.to_s
-    if javascript.empty?
-      result = ""
-    else
-      result = javascript.gsub(/(\\|<\/|\r\n|\342\200\250|\342\200\251|[\n\r"']|[`]|[$])/u, JS_ESCAPE_MAP)
-    end
+    result = if javascript.empty?
+               ''
+             else
+               javascript.gsub(/(\\|<\/|\r\n|\342\200\250|\342\200\251|[\n\r"']|[`]|[$])/u, JS_ESCAPE_MAP)
+             end
     javascript.html_safe? ? result.html_safe : result
   end
 
   alias j escape_javascript
 end
+# rubocop:enable Rails/OutputSafety
