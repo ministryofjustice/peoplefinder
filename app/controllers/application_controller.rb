10159edb67900c24c29a0bec3c6d32e0140f76ee
willmcb <william.mcbrien@digital.justice.gov.uk>
2020-09-09 16:34:57 +0100

    Add before_action :set_paper_trail_whodunnit to application_controller
    Breaking change that needed to be addressed see:
    
    https://github.com/paper-trail-gem/paper_trail/blob/master/CHANGELOG.md#500-2016-05-02

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   before_action :ensure_user
+  before_action :set_paper_trail_whodunnit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
