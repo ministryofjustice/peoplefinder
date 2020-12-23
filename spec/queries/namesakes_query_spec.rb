b2fe6319ce24a86dac80fe27af7235f0ad9a58d2
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2020-12-23 17:34:06 +0000

    Make parens consistent with new SQL

--- spec/queries/namesakes_query_spec.rb
+++ spec/queries/namesakes_query_spec.rb
@@ -31,7 +31,7 @@ describe NamesakesQuery do
       %q{
         SELECT "people".*
         FROM "people"
-        WHERE ("people"."id" IS NOT NULL)
+        WHERE "people"."id" IS NOT NULL
         AND (
           (LOWER(surname) = 'richards' AND LOWER(given_name) = 'stephen')
         OR
