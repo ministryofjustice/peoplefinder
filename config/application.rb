9485cf7e66b6950980c5b888c70472625cb005a2
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2021-02-12 15:08:53 +0000

    Remove redundant block from config

--- config/application.rb
+++ config/application.rb
@@ -85,10 +85,6 @@ module Peoplefinder
       # To activate performance monitoring, set one of these options.
       # We recommend adjusting the value in production:
       config.traces_sample_rate = 0.5
-      # or
-      config.traces_sampler = lambda do |context|
-        true
-      end
     end
   end
 
