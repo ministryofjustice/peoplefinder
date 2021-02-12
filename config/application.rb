4afe2ab75c7cf1489d39da49b4b7cb57b41a2407
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2021-02-12 16:03:25 +0000

    Performance monitoring isn't available on our plan :(

--- config/application.rb
+++ config/application.rb
@@ -81,10 +81,6 @@ module Peoplefinder
     Sentry.init do |config|
       config.dsn = ENV['SENTRY_DSN']
       config.breadcrumbs_logger = [:active_support_logger]
-
-      # To activate performance monitoring, set one of these options.
-      # We recommend adjusting the value in production:
-      config.traces_sample_rate = 0.5
     end
   end
 
