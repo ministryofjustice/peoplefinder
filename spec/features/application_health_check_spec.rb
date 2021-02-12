30fd35d03a7a5827d7a5ca0baabb68684857514e
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2021-02-12 22:39:02 +0000

    update db check method
    Alternative is to use
    ActiveRecord::Migrator.current_version if defined?(ActiveRecord)
    but this is a little fiddly to test and other apps use `connection.active?`

--- spec/features/application_health_check_spec.rb
+++ spec/features/application_health_check_spec.rb
@@ -15,7 +15,7 @@ describe 'healthcheck.json' do
   end
 
   it 'when there are component errors' do
-    allow(ActiveRecord::Base).to receive(:connected?).and_return(false)
+    allow(ActiveRecord::Base.connection).to receive(:active?).and_return(false)
 
     visit '/healthcheck.json'
 
