8d2728a3d7954c092e54c930b631fb513c14ed4f
ymao2 <yikang.mao@digital.justice.gov.uk>
2021-12-21 12:32:09 +0000

    Deactivate CSS assets sass compression to solve the css issues(alphagov/finder-frontend/issues/1124)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,9 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.sass.style = :compressed
+  config.sass.line_comments = false
+  config.assets.css_compressor = nil
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
