44b66807929723ad0adce5143dcd5ed5a177438e
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2022-09-13 09:43:01 +0100

    Reinstate raise_runtime_errors config param

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.raise_runtime_errors = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
