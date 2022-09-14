f13464b3a7f441625bd40e3ffd77f56e4fb288c4
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2022-09-14 12:38:56 +0100

    Remove trailing comma

--- config/initializers/webmock.rb
+++ config/initializers/webmock.rb
@@ -2,7 +2,7 @@ if Rails.env.test?
   require 'webmock'
   allowed_connect = [
     'chromedriver.storage.googleapis.com',
-    Rails.application.config.elastic_search_url,
+    Rails.application.config.elastic_search_url
   ]
   WebMock.disable_net_connect!(allow: allowed_connect)
   WebMock.allow_net_connect!(net_http_connect_on_start: true)
