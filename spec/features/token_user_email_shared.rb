1c6f864629f5e822ee15f3de26207574da9cc66b
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-05-10 14:16:15 +0100

    Remove unused variable

--- spec/features/token_user_email_shared.rb
+++ spec/features/token_user_email_shared.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.shared_examples "it received a valid request from" do
   it "and sends the email" do
-    delivery_count = sent_to.blank? ? 0 : 1
     visit '/'
     fill_in 'token_user_email', with: email
     click_button 'Request link'
