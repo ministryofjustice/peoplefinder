17edd1b641b471be4f075a40ea6ba5f056fbe580
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-10-13 13:24:45 +0100

    Add manifest file

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,9 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+//= link govuk-template.css
+//= link govuk-template-ie6.css
+//= link govuk-template-ie7.css
+//= link govuk-template-ie8.css
+//= link govuk-template-print.css
+//= link fonts.css
