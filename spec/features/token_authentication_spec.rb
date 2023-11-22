49696241d29442211a4ed7373f72f913a5ca9935
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-11-22 13:39:24 +0000

    reinstate removed code

--- spec/features/token_authentication_spec.rb
+++ spec/features/token_authentication_spec.rb
@@ -101,7 +101,7 @@ describe "Token Authentication" do
   end
 
   context "when logging in with a valid token" do
-    let(:ff31) { "Mozilla/5.0 (Windows NT 5.2; rv:31.0) Firefox/31.0" }
+    let(:ff31) { "Mozilla/5.0 (Windows NT 5.2; rv:31.0) Gecko/20100101 Firefox/31.0" }
     let(:ie6) { "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.648; .NET CLR 3.5.21022)" }
     let(:token) { create(:token) }
 
