93a3f4a3d6efef2560565f762ce3490591396f1b
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-11-22 15:48:13 +0000

    reinstate file

---
+++ spec/support/pages/management.rb
@@ -0,0 +1,10 @@
+module Pages
+  class Management < Base
+    set_url "/admin"
+
+    element :generate_link, ".generate-link > a"
+    element :download_link, ".download-link > a"
+    element :bulk_uploader_link, "#bulk-uploader > a"
+    element :audit_trail_link, "#audit-trail > a"
+  end
+end
