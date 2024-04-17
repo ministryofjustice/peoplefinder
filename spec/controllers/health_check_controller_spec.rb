fe9d21faf2306a30400824f737bbd8f5a9d60e03
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-04-17 16:22:15 +0100

    Fix healthcheck sentry logging (#934)
    * Log healthcheck calls
    
    * Return 503 status
    
    * refactor healthcheck
    
    * Update feature test
    
    * Serve all errors pages from app
    
    * Force 503 errors to be in app
    
    * Send string
    
    * Remove custom error setting
    
    * Use 500 status and send error reason to sentry
    
    * Add space
    
    * Remove config

--- spec/controllers/health_check_controller_spec.rb
+++ spec/controllers/health_check_controller_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe HealthCheckController, type: :controller do
   describe "#index" do
-    let(:passing) { double(available?: true, accessible?: true) } # rubocop:disable RSpec/VerifiedDoubles
-    let(:failing) { double(available?: false, accessible?: false) } # rubocop:disable RSpec/VerifiedDoubles
+    let(:passing) { double(available?: true, accessible?: true, errors: nil) } # rubocop:disable RSpec/VerifiedDoubles
+    let(:failing) { double(available?: false, accessible?: false, errors: "error reason") } # rubocop:disable RSpec/VerifiedDoubles
 
     context "when a problem exists" do
       before do
@@ -14,7 +14,7 @@ RSpec.describe HealthCheckController, type: :controller do
       end
 
       it "returns status bad gateway" do
-        expect(response.status).to eq(503)
+        expect(response.status).to eq(500)
       end
 
       it "returns the expected response report" do
@@ -23,7 +23,7 @@ RSpec.describe HealthCheckController, type: :controller do
       end
 
       it "sends report to Sentry" do
-        expect(Sentry).to receive(:capture_message)
+        expect(Sentry).to receive(:capture_message).with(String)
         get :index
       end
     end
