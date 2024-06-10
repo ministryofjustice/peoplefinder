e225e4774d0ea42318b5fd44c47eab67e594a110
N.Preddy <nick.preddy@digital.justice.gov.uk>
2024-06-10 12:43:20 +0100

    Update 20240522140110_create_external_users.rb

--- db/migrate/20240522140110_create_external_users.rb
+++ db/migrate/20240522140110_create_external_users.rb
@@ -1,12 +1,7 @@
 class CreateExternalUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :external_users do |t|
-      t.text :given_name
-      t.text :surname
-      t.text :slug
       t.text :email
-      t.text :company
-      t.boolean :admin
 
       t.timestamps
     end
