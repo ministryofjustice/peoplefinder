752f2a176a4d7f9aa636026c80670da11ca59d1a
N.Preddy <nick.preddy@digital.justice.gov.uk>
2024-06-11 09:53:21 +0100

    schema updated

--- spec/models/external_user_spec.rb
+++ spec/models/external_user_spec.rb
@@ -3,12 +3,7 @@
 # Table name: external_users
 #
 #  id           :integer  not null, primary key
-#  given_name   :text
-#  surname      :text
-#  slug         :string
 #  email        :text
-#  company      :text
-#  admin        :boolean  default(FALSE)
 #  created_at   :datetime
 #  updated_at   :datetime
 #
