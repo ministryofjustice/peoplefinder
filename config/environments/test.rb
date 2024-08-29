748d2dc1f3ec2a57cb5a70e5cbcfe645b4da9942
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-08-29 11:00:03 +0100

    Remove deprecations (#983)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,9 +28,6 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
-
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
